c055371ef5ffe849b110417d201919c381062bea
Kaela Clement <stephanie.kaela@gmail.com>
2018-11-11 05:19:55 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,13 +2,35 @@ module Players
 
   class Computer < Player
 
+    WINNING_SPACES = [
+      ["1", "5", "9"],
+      ["3", "5", "7"],
+      ["4", "5", "6"],
+      ["2", "5", "8"],
+      ["1", "2", "3"],
+      ["7", "8", "9"],
+      ["1", "4", "7"],
+      ["3", "6", "9"]
+    ]
+
     def move(board)
-      output = rand(1..9)
-      if board.valid_move?(output)
-        output.to_s
+      corners = ["1", "3", "7", "9"]
+      edges = ["2", "4", "6", "8"]
+      if board.turn_count <= 1
+        turn_1 = corners.detect{|corner| board.valid_move?(corner)}
+      elsif board.turn_count == 2 || board.turn_count == 3
+        if board.valid_move?("5")
+          turn_2 = "5"
+        else
+          turn_2 = corners.detect{|corner| board.valid_move?(corner)}
+        end
       else
-        move(board)
+        turn_array = WINNING_SPACES.detect do |winning_array|
+          winning_array.all?{|cell| board.position(cell) == self.token || board.position(cell) == " "}
+        end
+        turn_array.detect{|cell| board.valid_move?(cell)}
       end
+
     end
 
   end
