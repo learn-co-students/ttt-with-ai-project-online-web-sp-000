197ac73d6e2ab8bb306538c3b3f08d1344ec8ffa
Kaela Clement <stephanie.kaela@gmail.com>
2018-11-11 20:03:49 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,17 +2,6 @@ module Players
 
   class Computer < Player
 
-    WINNING_SPACES = [
-      ["1", "5", "9"],
-      ["3", "5", "7"],
-      ["4", "5", "6"],
-      ["2", "5", "8"],
-      ["1", "2", "3"],
-      ["7", "8", "9"],
-      ["1", "4", "7"],
-      ["3", "6", "9"]
-    ]
-
     def move(board)
       corners = ["1", "3", "7", "9"]
       if board.turn_count <= 1
@@ -24,16 +13,14 @@ module Players
           turn_2 = corners.detect{|corner| board.valid_move?(corner)}
         end
       else
-        this_turn = WINNING_SPACES.detect do |winning_array|
-          winning_array.detect{|cell| board.valid_move?(cell)}
-        end
-        if this_turn.class == Array
-          this_turn.detect{|cell| board.valid_move?(cell)}
+        output = rand(1..9)
+        if board.valid_move?(output)
+          output
         else
-          board.cells.shuffle.detect{|cell| board.valid_move?(cell)}
+          move(board)
         end
       end
-    end
+    end # end of method
 
   end
 
