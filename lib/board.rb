3173ff6013cc5e2cbd384a08675a2e4a3d17e67a
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-07 06:31:44 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,47 @@
+class Board
+  attr_accessor :cells
+  def initialize
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def reset!
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def position(user_input)
+    @cells[user_input.to_i - 1]
+  end
+
+  def full?
+    @cells.all? {|x| x != " "}
+  end
+
+  def turn_count
+    @cells.collect {|x| x == " "}.size / 3
+  end
+
+  def taken?(position)
+    cells[position.to_i - 1] == "X" || cells[position.to_i - 1] == "O"
+  end
+
+  def valid_move?(position)
+    !(taken?(position))  && (0..9).include?(position.to_i - 1)
+  end
+
+  #update updates the cells in the board with the player token according to the input
+  token = "O"
+
+  def update(position, token)
+    cells[position.to_i - 1] = token
+    display
+  end
+
+end
---
+++ lib/player.rb
@@ -0,0 +1,9 @@
+class Player
+  attr_reader :token
+  #initialize accepts a token to assign
+  def initialize(token)
+    @token = token
+  end
+
+
+end
