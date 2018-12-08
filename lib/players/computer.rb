6db1fff2af1c2408ef3f5d0e861d04dd23e44b0a
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-08 06:19:38 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,6 +1,10 @@
 module Players
   class Computer < Player
     def move(board)
+      turn_number = @board.turn_count
+      if turn_number == 0
+        @board.cells[4] = player.token
+      end
 
     end
   end
