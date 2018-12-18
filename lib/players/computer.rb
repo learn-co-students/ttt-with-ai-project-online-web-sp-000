ad8bf3844501641e8ef0dbc855a912b23910c290
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-12-18 11:27:36 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,10 +2,6 @@ module Players
 
   class Computer < Player
 
-    WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
-
-    CORNERS = [0,2,8,6]
-
     def move(board)
       if !board.taken?("5")
         input = "5"
@@ -14,8 +10,6 @@ module Players
       elsif !board.taken?("2") || !board.taken?("4") || !board.taken?("6") || !board.taken?("8")
         input = ["2", "4", "6", "8"][rand(4)]
       end
-#      choices = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-#      input = choices[rand(9)]
     end
 
   end
