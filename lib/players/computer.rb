b376ccbc0eda06a4a8c576b3d32dcc03d072e567
Daniel Dawson <dannyldawson@gmail.com>
2019-01-01 21:48:30 -0800

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -16,7 +16,7 @@ module Players
         move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
 
       # If you went second (and took the middle) and the other player has occupied opposing corner squares, blow up the attempted trap by taking a side square.
-      elsif board.turn_count == 3 && (board.position(1) == board.position(9) || board.position(3) == board.position(7))
+    elsif board.turn_count == 3 && (board.position("1") == board.position("9") || board.position("3") == board.position("7"))
         move = "2"
 
       # From here on, run through the WIN_COMBINATIONS array, checking whether any of the combinations have two squares filled with the same token and a third, empty square.
@@ -24,17 +24,17 @@ module Players
         Game::WIN_COMBINATIONS.detect do |cmb|
 
           # First, check whether you have any chances to win, since it doesn't matter whether the opponent has a chance to win if you can win first.
-          if cmb.select{|i| board.position(i+1) == token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
+          if cmb.select{|i| board.position((i+1).to_s) == token}.size == 2 && cmb.any?{|i| board.position((i+1).to_s) == " "}
             move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
 
           # If you can't play any winning moves, play a move to block the opponent from winning.
-          elsif cmb.select{|i| board.position(i+1) != " " && board.position(i+1) != token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
-            move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
+          elsif cmb.select{|i| board.position((i+1).to_s) != " " && board.position((i+1).to_s) != token}.size == 2 && cmb.any?{|i| board.position((i+1).to_s) == " "}
+            move = cmb.select{|i| !board.taken?((i+1).to_s)}.first.to_i.+(1).to_s
           end
         end
 
         # If none of the WIN_COMBINATIONS patterns have two squares taken by the same token and a third empty square, play into the first open square you find, first checking corners and then checking sides.
-        move = [1, 3, 7, 9, 2, 4, 6, 8].detect{|i| !board.taken?(i)}.to_s if move == nil
+        move = [1, 3, 7, 9, 2, 4, 6, 8].detect{|i| !board.taken?(i.to_s)}.to_s if move == nil
       end
       move
     end
