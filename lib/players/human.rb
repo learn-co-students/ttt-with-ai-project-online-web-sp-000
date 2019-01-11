3fd86731b36f40f052c0fb26497efdded775a802
Meisuleen Kased <meisuleen@gmail.com>
2019-01-11 16:27:14 +0000

    Done.

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,7 +1,7 @@
 module Players
   class Human < Player
     def move(board)
-      puts "Please enter a number from 1-9"  #Is this only for Players::Computer?
+      puts "Please enter a number from 1-9"
       gets.strip
     end
   end
