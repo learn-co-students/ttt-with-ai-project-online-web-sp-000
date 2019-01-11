fa8619302b77525295180153bf67cdbb90212076
Meisuleen Kased <meisuleen@gmail.com>
2019-01-11 18:14:51 +0000

    Done.

---
+++ lib/game.rb
@@ -0,0 +1,16 @@
+class Game
+  ##`WIN_COMBINATIONS`
+# Define a `WIN_COMBINATIONS` constant within the `TicTacToe` class, and set it equal to a nested array filled with the
+# index values for the various winning combinations possible in Tic Tac Toe.
+
+WIN_COMBINATIONS =
+ [[0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]]
+
+end
