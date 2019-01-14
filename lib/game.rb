d04b44a991eb5bfef4115c059d0456b743943117
Meisuleen Kased <meisuleen@gmail.com>
2019-01-14 02:51:33 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -5,72 +5,73 @@ class Game
   attr_accessor :board, :player_1, :player_2
 
   WIN_COMBINATIONS =
-    [[0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]]
+     [[0,1,2],
+     [3,4,5],
+     [6,7,8],
+     [0,3,6],
+     [1,4,7],
+     [2,5,8],
+     [0,4,8],
+     [2,4,6]]
 
-  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
-    #player_1 needs to access the Players module and then the Human who is playing as "X", then makes a new instance when needed
-    #player_2 needs to access the Players module and then the other Human who is playing as "O", then makes a new instance when needed
-    @board = board
-    @player_1 = player_1
-    @player_2 = player_2
-  end
+   def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+     #player_1 needs to access the Players module and then the Human who is playing as "X", then makes a new instance when needed
+     #player_2 needs to access the Players module and then the other Human who is playing as "O", then makes a new instance when needed
+     @board = board
+     @player_1 = player_1
+     @player_2 = player_2
+   end
 
-  def current_player #returns the correct player, X, for the third move
-    self.board.turn_count.even? ? player_1 : player_2
-  end
+   def current_player #returns the correct player, X, for the third move
+     self.board.turn_count.even? ? player_1 : player_2
+   end
 
-  def won?
-    WIN_COMBINATIONS.detect do |combinations|
-      #@board.cells[combinations[0]] == @board.cells[combinations[1]] &&
-      #@board.cells[combinations[1]] == @board.cells[combinations[2]] &&
-      #@board.taken?(combinations[0] +1)
-      combinations.all? {|cell| board.cells[cell] == player_1.token} || combinations.all? { |cell| board.cells[cell] == player_2.token }
-    end
-  end
-
-  def draw?
-    @board.full? && !won?
-  end
-
-  def over?
-    board.full? || won? || draw?
-  end
-
-  def winner
-    if won?
-      combination = won?
-      @board.cells[combination[0]]
-    end
-  end
-
-  def turn
-    puts "Please enter 1-9:"
-    user_input = current_player.move(board) #player_1 plays the first round
-
-    if board.valid_move?(user_input)   #determines if valid_move
-      board.update(user_input, current_player)  #switches to the player_2
-      board.display
-    else
-      turn  # takes a turn
-    end
-  end
-
-
- def play
-     while !over?
-       turn
-     end
-     if won?
-       puts "Congratulations #{winner}!"
-     else
-       puts "Cat's Game!"
+   def won?
+     WIN_COMBINATIONS.detect do |combinations|
+       #@board.cells[combinations[0]] == @board.cells[combinations[1]] &&
+       #@board.cells[combinations[1]] == @board.cells[combinations[2]] &&
+       #@board.taken?(combinations[0] +1)
+       combinations.all? {|cell| board.cells[cell] == player_1.token} || combinations.all? { |cell| board.cells[cell] == player_2.token }
      end
    end
+
+   def draw?
+     @board.full? && !won?
+   end
+
+   def over?
+     board.full? || won? || draw?
+   end
+
+   def winner
+     if won?
+       combination = won?
+       @board.cells[combination[0]]
+     end
+   end
+
+   def turn
+     puts "Please enter 1-9:"
+     user_input = current_player.move(board) #player_1 plays the first round
+
+     if board.valid_move?(user_input)   #determines if valid_move
+       board.update(user_input, current_player)  #switches to the player_2
+       board.display
+     else
+       turn  # takes a turn
+     end
+   end
+
+
+     def play
+      while !over?
+        turn
+      end
+      if won?
+        puts "Congratulations #{winner}!"
+      else
+        puts "Cat's Game!"
+      end
+    end
+
  end
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,5 +1,5 @@
 # The idea of being efficient wanting to check the win combinations is good…just keep in mind that
-#  the collection is not very big, the logic of checking WHICH combinations to check might outweigh 
+#  the collection is not very big, the logic of checking WHICH combinations to check might outweigh
 #  just checking all of those. You might be able to leverage one of ruby’s nice efficient methods like any?
 
 # opt for a win combo rather than a block combo - more efficient
@@ -9,9 +9,37 @@
 
 # take middle cell if empty
 # after the first move, make another move based on empty cells
-# once everyone has 2 tokens on the board, you use a win combo or a block combo and make a move 
+# once everyone has 2 tokens on the board, you use a win combo or a block combo and make a move
 # =>iterate over win combos and block combo to check for a win use detect enumerable
 
 # “X”, “X”, “”
 # “O”,”O”,” “
 # “ “, “X”, “ “
+
+module Players
+  class Computer < Player
+    def move(board)
+      #first move
+      # if "5" in board is not taken
+      #move = "5"
+
+      #second move
+      #elsif board.turn_count
+      # if "5" is not taken then take it
+
+      # if "5" is taken the take "3"
+
+      #third move
+
+    end
+
+        #Natasha does lines 1-20
+        #Patrice does lines 23-28
+        #Meisuleen does lines 30-42
+
+
+
+
+
+  end
+end
