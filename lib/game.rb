107efd6eb393ae2c0d64c0a9c9a5528b2a7aaf81
Diana Badas <diana.badas@gmail.com>
2019-02-07 16:14:27 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -20,10 +20,74 @@ class Game
   ]
 
   def current_player
-    if @board.turn_count.even?
+    if board.turn_count.even?
       player_1
     else
       player_2
+    end
+  end
+
+  def won?
+    WIN_COMBINATIONS.each do |win_combination|
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+
+      position_1 = board.cells[win_index_1]
+      position_2 = board.cells[win_index_2]
+      position_3 = board.cells[win_index_3]
+
+      if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 =="O" && position_2 == "O" && position_3 == "O"
+        return win_combination
+      end
+    end
+    else
+      false
+  end
+
+  def draw?
+    if board.full? && !won?
+      true
+    else
+      false
+    end
+  end
+
+  def over?
+    if board.full? || won? || draw?
+      true
+    else
+      false
+    end
+  end
+
+  def winner
+    if won_result = won?
+      board.cells[won_result.first]
+    end
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    input = current_player.move(board)
+    if board.valid_move?(input)
+      board.update(input, current_player)
+    else
+      puts "Please enter 1-9:"
+      board.display
+      turn
+    end
+    board.display
+  end
+
+  def play
+    while !over?
+      turn
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
     end
   end
 
