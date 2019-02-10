0bc27f703eac3c131b5d821f621f0642ff4ba280
karen ercoli <karenercoli@gmail.com>
2019-02-10 14:36:10 +0000

    Done.

---
+++ lib/game.rb
@@ -0,0 +1,53 @@
+require "pry"
+
+class Game
+  attr_accessor :board, :player_1, :player_2
+  WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+]
+
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = board
+  end
+
+  def current_player
+    #i need to return player_2's token
+      if  @board.turn_count % 2 == 0
+        return player_1
+      else
+        return player_2
+      end
+  end
+
+def won?
+WIN_COMBINATIONS.detect do |combination|
+  @board.cells[combination[0]] == @board.cells[combination[1]] &&
+  @board.cells[combination[1]] == @board.cells[combination[2]]
+end
+end
+
+def draw?
+!won? && @board.full?
+end
+
+def over?
+  won? || draw?
+end
+
+def winner
+if won?
+  current_player.token
+end
+end
+
+
+end
