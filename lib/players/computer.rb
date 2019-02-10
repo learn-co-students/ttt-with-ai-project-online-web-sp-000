c3cd27412619a18b13941b07649d1c7699f833f2
karen ercoli <karenercoli@gmail.com>
2019-02-10 20:41:55 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,26 +5,26 @@ module Players
 
 
     def move(board)
-      if !@board.taken?(1)
-        move(1)
-        binding.pry
-      elsif !@board.taken?(2)
-        move(2)
-      elsif !@board.taken?(3)
-        move(3)
-      elsif !@board.taken?(4)
-        move(4)
-      elsif !@board.taken?(5)
-        move(5)
-      elsif !@board.taken?(6)
-        move(6)
-      elsif !@board.taken?(7)
-        move(7)
-      elsif !@board.taken?(8)
-        move(8)
-      elsif !@board.taken?(9)
-        move(9)
-    end
+      rand(1..9).to_s
+      # 1
+      #
+      # elsif !(board.taken?(2))
+      #   move(2)
+      # elsif !(board.taken?(3))
+      #   move(3)
+      # elsif !(board.taken?(4))
+      #   move(4)
+      # elsif !(board.taken?(5))
+      #   move(5)
+      # elsif !(board.taken?(6))
+      #   move(6)
+      # elsif !(board.taken?(7))
+      #   move(7)
+      # elsif !(board.taken?(8))
+      #   move(8)
+      # elsif !(board.taken?(9))
+      #   move(9)
+    #end
   end
   end
 end
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -2,6 +2,7 @@ module Players
   class Human < Player
     def move(input)
       input = gets.chomp
+
     end
   end
 end
