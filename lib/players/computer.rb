d66e731cc945897edd3ceb82c4e77684478b8a54
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-18 20:18:49 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -20,47 +20,52 @@ class Computer < Player
   end
 
   def psuedo_gets(board)
-    close(board)
     gets_position = 0
     open_spot = 0
-    if close(board) == []
-      random = rand(0..8)
-        if board.valid_move?(random+1)
-          gets_position = random
-        else
-          psuedo_gets(board)
-        end
-    else
-      combo_index = close(board).sample[1]
+    # if close(board) == []
+    #   random = rand(0..8)
+    #     if board.valid_move?(random+1)
+    #       gets_position = random
+    #     else
+    #       psuedo_gets(board)
+    #     end
+    #else
+    #binding.pry
+        combo_index = close(board).sample[1]
         CLOSE_COMBINATIONS[combo_index].each_with_index do |value, index|
-            if !board.taken?(index+1) && board.valid_move?(index+1)
+            if board.valid_move?(index+1)
               open_spot = index
+              gets_position = CLOSE_COMBINATIONS[combo_index][open_spot]
+              gets_position += 1
+            else
+              random = rand(1..9)
+              if board.valid_move?(random)
+                gets_position = random
+              end
             end
-        end
-        gets_position = CLOSE_COMBINATIONS[combo_index][open_spot]
     end
-    gets_position += 1
     puts gets_position
     gets_position
 end
 
+
 def close(board)
-#binding.pry
     result = []
     CLOSE_COMBINATIONS.each_with_index do |sub_array, index|
 #these return the numbers/indexes associated with a winning combination
-      index_1 = sub_array[0]
-      index_2 = sub_array[1]
-      index_3 = sub_array[2]
+      number_1 = sub_array[0]
+      number_2 = sub_array[1]
+      number_3 = sub_array[2]
 
-#these tell you the values associated with a winning combination
-      board_index_1 = board.cells[index_1]
-      board_index_2 = board.cells[index_2]
-      board_index_3 = board.cells[index_3]
+#these tell you the values associated with an index
+      board_index_1 = board.cells[number_1]
+      board_index_2 = board.cells[number_2]
+      board_index_3 = board.cells[number_3]
 
-      if (board_index_1 == board_index_2 && !board.taken?(index_3+1) && board.valid_move?(index_3+1)) ||
-        (board_index_3 == board_index_2 && !board.taken?(index_1+1) && board.valid_move?(index_1+1)) ||
-        (board_index_3 == board_index_1 && !board.taken?(index_2+1) && board.valid_move?(index_2+1))
+#this checks to see if 2 of the values are the same, and if the last value is open && valid
+      if ((board_index_1 == board_index_2) && board.valid_move?(number_3+1)) ||
+        ((board_index_3 == board_index_2) && board.valid_move?(number_1+1)) ||
+        ((board_index_3 == board_index_1) && board.valid_move?(number_2+1))
             result << [sub_array, index]
       end
     end
