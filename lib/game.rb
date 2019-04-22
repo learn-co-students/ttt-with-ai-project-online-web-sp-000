0011990242843e64b43ef17a6e064d38003b2685
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-22 03:39:28 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -43,6 +43,7 @@ class Game
   def over?
     @board.full? || draw? || won?
   end 
+
   
   def winner 
     #won? returns winning combo 
@@ -52,7 +53,7 @@ class Game
   end 
   
   def turn 
-    @board.display
+     @board.display
     player = current_player
     puts "Please enter 1-9:"
     current_move = player.move(@board)
@@ -70,7 +71,26 @@ class Game
     turn until over?    
     
     @board.display
-    if won?
+   player = current_player
+  current_move = player.move(@board)
+  if !@board.valid_move?(current_move)
+    turn
+  else
+     @board.update(current_move, player)
+    puts "#{player.token} moved #{current_move}"
+    @board.display
+    puts "\n\n"
+  end 
+end 
+ 
+  def play
+    while !over?     
+      turn
+    end
+    
+    @board.display
+
+     if won?
       puts "Congratulations #{winner}!"
     elsif draw?
       puts "Cat's Game!"
