dbf38bd7a61c829458e32d03e2fba41e27d1a17b
Yingqi Chen <chanwk18@hotmail.com>
2019-04-30 16:55:19 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,10 +2,47 @@ module Players
 
 class Computer < Player
 
+  ##mostly reference from the online source, but I change something on line 19,check the comment there
   def move(board)
+      move = nil
 
-  ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample
+      # When going first, take the middle square. When going second, take the middle square if it isn't yet taken.
+      if !board.taken?(5)
+        move = "5"
+
+      # If going second and the middle square is taken, take the upper-left corner square.
+      elsif board.turn_count == 1
+        move = "1"
+
+      # If you went first (and took the middle), take a corner square with your second move.
+      elsif board.turn_count == 2
+        move = [1, 3, 7, 9].select{|i| !board.taken?(i)}.sample.to_s #at first instead of select, it uses find/detect
+        #so when i choose two computers playing mode, they will always use the same strategy so it will be predictable and boring!
+        #now at least i add some kinds of randomness to it by adding select and then get a sample from it
+
+
+      # If you went second (and took the middle) and the other player has occupied opposing corner squares, blow up the attempted trap by taking a side square.
+      elsif board.turn_count == 3 && (board.position(1) == board.position(9) || board.position(3) == board.position(7))
+        move = "2"
+
+      # From here on, run through the WIN_COMBINATIONS array, checking whether any of the combinations have two squares filled with the same token and a third, empty square.
+      else
+        Game::WIN_COMBINATIONS.detect do |cmb|
+
+          # First, check whether you have any chances to win, since it doesn't matter whether the opponent has a chance to win if you can win first.
+          if cmb.select{|i| board.position(i+1) == token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
+            move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
+
+          # If you can't play any winning moves, play a move to block the opponent from winning.
+          elsif cmb.select{|i| board.position(i+1) != " " && board.position(i+1) != token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
+            move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
+          end
+        end
+
+        # If none of the WIN_COMBINATIONS patterns have two squares taken by the same token and a third empty square, play into the first open square you find, first checking corners and then checking sides.
+        move = [1, 3, 7, 9, 2, 4, 6, 8].detect{|i| !board.taken?(i)}.to_s if move == nil
+      end
+      move
+    end
   end
-
-end
 end
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,8 +1,9 @@
+
 module Players
 class Human < Player
-  def move(board)
-    input=gets.chomp
-  end
+def move(board)
+input=gets.chomp
+end
 
 end
 end
