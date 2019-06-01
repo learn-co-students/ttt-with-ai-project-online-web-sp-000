7fec4712085ac82d4e938778a6aae65257afcaf3
Lauren McCoy <lmccoy8911@gmail.com>
2019-06-01 18:27:16 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -89,5 +89,4 @@ class Game
   end 
   
  
-  
 end 
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,8 +2,11 @@ require 'pry'
 module Players 
   class Computer < Player 
    def move(board) 
-      
+     binding.pry 
+      if board.cells[combo[0]].empty?
+        self.turn 
+      end 
    end 
      
-  end
+  end 
 end 
