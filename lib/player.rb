4091c1fde848713d2da3918ec9297482a451c0a8
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-06-15 19:07:31 +0000

    Done.

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,7 @@
 class Player
-  extend Player::Human
-  
   attr_reader :token
   
   def initialize(token)
     @token = token
   end
-  
 end
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,3 +1,11 @@
-Module Human
-  Human < Player
+module Players
+  class Human < Player
+    
+  def move(board) 
+    puts "Please enter 1-9"
+    board = gets.strip
+  end
+  
+  
+ end
 end
