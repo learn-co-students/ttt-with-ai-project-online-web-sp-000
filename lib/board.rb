916f1ddcf8e3b08b82eb8e944814302ae21f5166
Karen V <karenvo@usc.edu>
2019-06-16 05:07:31 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,47 @@
+require 'pry'
+class Board
+  attr_accessor :cells
+  @@cells = []
+  def initialize
+    #@cells = [" " ," " ," " ," " ," " ," " ," " ," " ," "]
+    self.cells = Array.new(9, " ")
+  end
+  def reset!
+    # can reset the state of the cells in the board
+    self.cells = Array.new(9, " ")
+  end
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+  def position(user_input)
+    self.cells[user_input.to_i - 1]
+  end
+  def full?
+    # returns true for a full board
+    # returns false for in-progress game
+    self.cells.all?{|cell| cell == "X" || cell == "O" }
+  end
+  def turn_count
+    # returns the amount of turns based on cell value
+    self.cells.count("X") + self.cells.count("O")
+  end
+  def taken?(position)
+    # returns true if the position is X or O
+    # returns false if the position is empty or blank
+    #self.cell.each{ |position| position == "X" || position =="O" }
+    self.cells[position.to_i - 1] == "X" || self.cells[position.to_i - 1] == "O"
+  end
+  def valid_move?(position)
+    # returns true for user input between 1 - 9 that is not taken
+    move = position.to_i - 1
+    !taken?(move) and move < 9 and move > 0
+  end
+  def update
+    # updates the cells in the board with the player token according to the input
+  end
+end
+#binding.pry
