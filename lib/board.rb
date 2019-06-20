f8816a30391034e4740e80cd3bfd0c52dcaceabc
Andrew Silva <andrewsilva9@gmail.com>
2019-06-20 02:25:32 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,13 +1,19 @@
 class Board
   attr_accessor :cells
 
-  @@cells = []
-
-  def cells
-    @@cells
+  def initialize
+    reset!
   end
 
   def reset!
-    @@cells.clear
+    @cells = Array.new(9, " ")
+  end
+
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
   end
 end
