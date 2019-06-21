6ed14cd633fc40b6c7949024106180f16a5eb53d
Andrew Silva <andrewsilva9@gmail.com>
2019-06-21 02:16:12 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -16,4 +16,45 @@ class Board
     puts "-----------"
     puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
   end
+
+  def position(space, character)
+
+   @cells[space] = character
+  end
+
+ def full?
+   if @cells.include?(' ') || @cells.include?('')
+    return false
+   else
+    return true
+   end
+ end
+
+ def turn_count
+  counter = 0
+  @cells.each do |space|
+    if space == "X" || space == "O"
+      counter += 1
+    end
+  end
+  counter
+ end
+
+ def taken?(integer)
+  if @cells[integer.to_i] == "X" || @cells[integer.to_i] == "O"
+   true
+ else
+   false
+  end
+ end
+
+ def valid_move?(index)
+  if (index < 0 || index > 8)
+    false
+  elsif position_taken?(index)
+    false
+  else
+    true
+  end
+ end
 end
