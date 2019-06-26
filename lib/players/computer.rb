307fef153adaa63593c3d4581febae3cd1bf9daf
kevin lang <langkevina@gmail.com>
2019-06-26 00:10:24 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,7 +3,8 @@ module Players
   class Computer < Player
     def move(board)
       valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-      valid_moves.each do |item|
+
+      valid_moves.sample(9).each do |item|
         if board.valid_move?(item)
           # binding.pry
           return item
