3247b591cbd668e077a3075a07e0c80c5c392f75
Connor Cowan <ccowan003@comcast.net>
2019-07-19 03:20:37 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,52 @@
+require 'pry'
+
 class Game
-  def initialize(player_1 = "human", player_2 = "human")
+
+  attr_accessor :board, :player_1, :player_2           #provides access to the board, player_1, player_2
+
+  WIN_COMBINATIONS = [            #Defined class Constant array
+                      [0, 1, 2],
+                      [3, 4, 5],
+                      [6, 7, 8],
+                      [0, 3, 6],
+                      [1, 4, 7],
+                      [2, 5, 8],
+                      [0, 4, 8],
+                      [6, 4, 2]
+  ]
+  #Human class, has to be called inside its name space with Players::Human
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
     @player_1 = player_1
     @player_2 = player_2
+    @board = board
   end
 
+  #returns the correct player, X, for the third move
+  def current_player
+    board.turn_count.even?  ? player_1 : player_2         #call to #turn_count & #even? on it. Ternary Operator
+  end
+
+  #returns false for a draw with #find method
+  #returns the correct winning combination in the case of a win
+  def won?
+    WIN_COMBINATIONS.find do |combo|
+      board.cells[combo[0]] == board.cells[combo[1]] && board.cells[combo[1]] == board.cells[combo[2]] && board.cells[combo[0]] != " "
+    end
+  end
+
+  def draw?
+    board.full? && !won?              #calling #full? on board class
+  end
+
+  def over?
+    won? || draw?
+  end
+
+  def winner
+    board.cells[won?[0]] if won?        #return X when X won and vice versa
+  end
+
+  def turn
+
+  end
 end
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,6 +1,10 @@
-module Human::Players           #module inherits from Player Class  NAMESPACE HERE
-  def move(board, timer = 0)
-    puts "Please enter 1-9:"
-    gets.chomp
+module Players                      #define module Players
+  class Human < Player              #Human inherits from Player class. Used nested module NAMESPACING here for Human inside modules Players
+    def move(board, timer = 0)      #asks the user for input and returns it
+      puts "Please enter 1-9:"
+      gets.chomp
+    end
   end
 end
+
+#Now when calling Human class, you'll have to call it inside its name space with Players::Human
