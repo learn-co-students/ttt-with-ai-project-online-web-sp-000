bc7da826d13b3f7e87b98e5a1cd9643101f142f4
Connor Cowan <ccowan003@comcast.net>
2019-07-19 01:53:29 +0000

    Done.

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,5 +1,4 @@
-module Players           #module inherits from Player Class  NAMESPACE HERE
-  class Human < Player
+module Human::Players           #module inherits from Player Class  NAMESPACE HERE
   def move(board, timer = 0)
     puts "Please enter 1-9:"
     gets.chomp
