844cb7d3195d00dce244f2b6051dd5aff3f26b2b
Lindsey Wells <lindsey.wells84@gmail.com>
2019-08-01 19:26:08 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,12 @@
+require "pry"
 class Game
   
-  attr_accessor :board, :player_1, :player_2
+  attr_accessor :board, :player_1, :player_2, :input
   
-  def initialize (board)
-    @board = Board.new
-    @player_1 = Player.new(X)
-    @player_2 = Player.new(Y)
+  def initialize(player_1=Players::Human.new("X"), player_2=Players::Human.new("O"), board = Board.new)
+    @board = board
+    @player_1 = player_1
+    @player_2 = player_2
   end
   
   WIN_COMBINATIONS = [
@@ -18,5 +19,65 @@ class Game
     [0,4,8],
     [2,4,6]
     ]
+    
+    def current_player
+      if @board.turn_count % 2 == 0
+        @player_1
+      else
+        @player_2
+      end
+    end
+    
+    
+    def won?
+      WIN_COMBINATIONS.find do |combo|
+       if board.taken?(combo[0]+1) && @board.cells[combo[0]] == @board.cells[combo[1]] && @board.cells[combo[1]] == @board.cells[combo[2]]
+         return combo
+      else
+         false
+       end
+     end
+   end
+   
+   def draw?
+     true if !won? && board.full?
+   end
+   
+   def over?
+     true if draw? || won?
+   end
+   
+   def winner
+     if won?
+       winning_combo = won?
+     board.cells[winning_combo[0]]
+   end
+ end
+ 
+ def turn
+ puts "Please enter a number 1-9:"
+    @input = current_player.move(@board)
+    if @board.valid_move?(@input)
+      @board.update(@input, current_player)
+    else puts "Please enter a number 1-9:"
+      @board.display
+      turn
+    end
+    @board.display
+  end
+  
+  
+  def play
+    turn until over? == true
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
+  
+        
+  
+    
   
 end
