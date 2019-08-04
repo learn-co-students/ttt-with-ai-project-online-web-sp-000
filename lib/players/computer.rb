df5b1e4a4d904b4e4f263ee0f75bd87c2ff8a53a
Lindsey Wells <lindsey.wells84@gmail.com>
2019-08-04 19:10:34 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -13,7 +13,7 @@ module Players
      
     case board.turn_count
         when 0 #if computer is player 1, on 1st turn always try go in a corner
-          [1,3,7,9].sample.to_s
+          first_move = [1,3,7,9].sample.to_s
         
         when 1 #if computer is player 2, if player 1 has taken a corner on first move, choose the centre, or if centre has been taken, choose a corner otherwise choose randomly from free spaces
           if [1,3,7,9].any?{|corner| board.taken?(corner)}
@@ -34,6 +34,26 @@ module Players
             rand(1..9).to_s
           else "5"
           end
+          
+          when 3 # if opponent plays first 0 in center, choose diagonally opposite space to your first move. if they didnt chose center, place your X on an edge square (not a corner), not touching your first X.
+          if board.taken?(5)
+            if first_move == "1"
+              "9"
+              elsif first_move == "3"
+              "7"
+              elsif first_move == "7"
+              "3"
+              else
+              "1"
+            end
+          else
+             choice = [2,4,6,8].sample.to_s
+             until !board.taken?(choice)
+             choice = [2,4,6,8].sample.to_s
+            end
+            choice
+          end
+            
         
         else #choose a random free space
            random = rand(1..9).to_s
