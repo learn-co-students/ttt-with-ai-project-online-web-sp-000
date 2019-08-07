e76fb988a5679ded319a90109032eff3af67d253
Aglae Verduzco <aglae.cv@gmail.com>
2019-08-07 02:00:43 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -66,4 +66,59 @@ end
       puts "Cat's Game!"
     end
   end
+
+  def self.start
+    puts "Welcome to Tic-tac-toe!"
+    puts "How would you like to play?"
+    puts "0 = 0-player game has two computer players playing against each other with no interaction from the user."
+    puts "1 = 1-player game has a human playing against a computer."
+    puts "2 = 2-player game has two human players."
+    puts "Please select a number (0,1,2) "
+    input = gets.strip.to_i
+    if input == "0"
+      game = Game.new(Players::Computer.new("X"), Players::Computer.new("O"))
+      game.play
+    elsif input == "1"
+      puts "Choose token: X or O:"
+      token = gets.strip.upcase
+      if token == "X"
+        game = Game.new(Players::Human.new(token), Players::Computer.new("O"))
+        game.play
+      elsif token == "O"
+        game = Game.new(Players::Human.new(token), Players::Computer.new("X"))
+        game.play
+      else
+        puts "Please choose an X or O token only."
+        start
+        end
+      elsif input == "2"
+        puts "Choose token: X or O for player 1:"
+        token - gets.strip.upcase
+      if token == "X"
+        game = Game.new(Players::Human.new(token), Players::Human.new("O"))
+        game.play
+      elsif token == "O"
+        game = Game.new(Players::Human.new(token), Players::Human.new("X"))
+        game.play
+      else
+        puts "Please choose an X or O token only."
+        start
+        end
+      else
+      puts "Invalid input"
+      start
+      end
+    end
+
+    def play_again
+      puts "Play again? y/n:"
+      input = gets.strip.upcase
+    if input == "Y"
+      self.start
+    elsif input == "N"
+      exit
+    else
+      play_again
+    end
+  end
 end
