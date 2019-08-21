b983b8da05272451f1efc34e23b5108a5864b7f2
Chris Cheong <chrisxcheong@gmail.com>
2019-08-21 03:26:20 +0000

    Done.

--- lib/StartMenu.rb
+++ lib/StartMenu.rb
@@ -1,13 +1,15 @@
-def start
-  puts "Welcome to Tic Tac Toe!"
-  puts "How many players?"
-  puts "0, 1 , 2"
+class TicTacToe
+  def initialize
+    @token = []
   end
-  input
-  select_mode
-end
 
-def select_mode(input)
+  def start
+    puts "Welcome to Tic Tac Toe!"
+    user_input = get_user_input
+    select_mode(user_input)
+  end
+
+  def select_mode(input)
     if input == 0
       ai_v_ai
     elsif input == 1
@@ -15,11 +17,48 @@ def select_mode(input)
     elsif input == 2
       two_player
     else
-      puts "Please select 0, 1 or 2"
-      input
+      start
+    end
   end
-end
 
-def input
-    input = gets
+  def get_user_input
+      puts "Pease select how many players"
+      puts "0, 1, 2 "
+      user_input = gets
+      user_input.to_i
+  end
+
+  def ai_v_ai
+    comp_1 = Players::Computer.new("X")
+    comp_2 = Players::Computer.new("O")
+    new_game = Game.new(comp_1, comp_2)
+    new_game.play
+  end
+
+  def single_player
+    puts "which token would you like? X or O"
+    token = gets.chomp.upcase
+    computer_token(token)
+  end
+
+  def computer_token(token)
+      if token == "X"
+        player = Players::Human.new("X")
+        computer = Players::Computer.new("O")
+
+      elsif token == "O"
+        player = Players::Human.new("O")
+        computer = Player::Computer.new("X")
+      else
+        puts "Please choose X or O"
+        single_player
+      end
+  end
+
+  def two_player
+    puts "which token would you like? X or O"
+    token = gets.chomp.upcase
+    token = "X"? player_1="X" : player_2="X"
+  end
+
 end
