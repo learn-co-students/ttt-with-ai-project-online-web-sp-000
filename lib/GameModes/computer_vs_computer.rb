6899e878d2a9d6e31a5ffd2cdef042fea0370e68
Chris Cheong <chrisxcheong@gmail.com>
2019-08-21 04:11:06 +0000

    Done.

--- lib/GameModes/computer_vs_computer.rb
+++
@@ -1,6 +0,0 @@
-def ai_v_ai
-  comp_1 = Players::Computer.new("X")
-  comp_2 = Players::Computer.new("O")
-  new_game = Game.new(comp_1, comp_2)
-  new_game.play
-end
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,7 +2,6 @@ module Players
 
   class Computer < Player
     def move(board)
-      #board.cells.detect {|cell|input = cell == " "
       input = board.cells.index(" ")
       input +=1
       input.to_s
