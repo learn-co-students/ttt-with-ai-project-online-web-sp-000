649a843b570114bd257792e6b87f344abb1e8132
Alex Velasquez <jtinnesco@gmail.com>
2019-08-22 03:17:59 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -9,4 +9,11 @@ class Board
     self.cells = (Array.new(9, " "))
   end
 
+  def display #Prints the Board
+    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+    puts "-----------"
+    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+    puts "-----------"
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+  end
 end
