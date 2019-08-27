fb6bd1e408c8e47cc0a61f4326b541a5cf623734
Alex Velasquez <jtinnesco@gmail.com>
2019-08-27 03:29:42 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,84 @@
 class Game
-  attr_accessor :board
+  attr_accessor :board, :player_1, :player_2
+
+  WIN_COMBINATIONS = [
+                      [0,1,2],
+                      [3,4,5],
+                      [6,7,8],
+                      [0,3,6],
+                      [1,4,7],
+                      [2,5,8],
+                      [0,4,8],
+                      [6,4,2]
+  ]
+
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = board
+  end
+
+  def board
+    @board
+  end
+
+  def player_1
+    @player_1
+  end
+
+  def player_2
+    @player_2
+  end
+
+  def current_player
+    @board.turn_count % 2 == 0 ? player_1 : player_2
+  end
+
+  def won?
+    WIN_COMBINATIONS.detect do |winner|
+      @board.cells[winner[0]] == @board.cells[winner[1]] &&
+      @board.cells[winner[1]] == @board.cells[winner[2]] &&
+      (@board.cells[winner[0]] == "X" || @board.cells[winner[0]] == "O")
+    end
+  end
+
+  def draw?
+    @board.full? && !won?
+  end
+
+  def over?
+    won? || draw?
+  end
+
+  def winner
+    if winning_combo = won?
+      @winner = @board.cells[winning_combo.first]
+    end
+  end
+
+  def turn
+    board.display
+    input = current_player.move(board)
+
+    if board.valid_move?(input)
+      board.update(input, current_player)
+    else
+      puts "That is a valid move."
+      turn
+    end
+  end
+
+  def play
+    while !over?
+      turn
+    end
+
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat\'s Game!"
+    end
+    board.display
+  end
 
 end
