017c718dc0dac7780c4b16134d06eda9bfc5b6c2
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-09-27 21:38:34 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -8,16 +8,8 @@ module Players
         number = [1, 3, 7, 9].sample
         move = "#{number}"
       elsif board_oi.turn_count == 2
-        if board_oi.position(1) == "O" || board_oi.position(9) == "O"
-          number = [2, 3, 6, 4, 7, 8].detect {|i| !board_oi.taken?(i)}
-          move = "#{number}"
-        elsif board_oi.position(3) == "O" || board_oi.position(7) == "O"
-          number = [2, 1, 6, 4, 9, 8].detect {|i| !board_oi.taken?(i)}
-          move = "#{number}"
-        else
-          number = [1, 3, 7, 9].sample
-          move = "#{number}"
-        end
+        number = [1, 3, 7, 9].detect{|i| !board_oi.taken?(i)}
+        move = "#{number}"
         
       elsif board_oi.turn_count == 3
         Game::WIN_COMBINATIONS.each do |combo|
@@ -28,15 +20,26 @@ module Players
           elsif board_oi.position(combo[2]) == board_oi.position(combo[1]) && board_oi.position(combo[2]) == "X"
             move = "#{combo[0]}"
           end
+          if move == nil
+            number = [1, 3, 7, 9].detect{|i| !board_oi.taken?(i)}
+            move = "#{number}"
+          end
         end
-        if move = nil
-          number = [1, 3, 7, 9].sample
-          move = "#{number}"
+      elsif board
+        Game::WIN_COMBINATIONS.each do |combo|
+          if board_oi.position(combo[0]) == board_oi.position(combo[1]) && board_oi.position(combo[0]) == "X"
+            move = "#{combo[2]}"
+          elsif board_oi.position(combo[0]) == board_oi.position(combo[2]) && board_oi.position(combo[0]) == "X"
+            move = "#{combo[1]}"
+          elsif board_oi.position(combo[2]) == board_oi.position(combo[1]) && board_oi.position(combo[2]) == "X"
+            move = "#{combo[0]}"
+          end
+          if move == nil
+            number = [1, 3, 7, 9].detect{|i| !board_oi.taken?(i)}
+            move = "#{number}"
+          end
         end
-      else                           
-        number = board_oi.move_box.sample
-        move = "#{number}"
-        board_oi.update_move_box(number)
+        
       end
       move
     end
