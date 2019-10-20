d8baf43eea6ba68108920e4e935e6ddab125d822
Blythe Pollard <blytheepollard@gmail.com>
2019-10-20 22:39:42 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -20,8 +20,11 @@ class Board
     puts "-----------"
     puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
   end
-
   #display method prints current state of board
+  
+  def position(input)
+    new_position = @cells[input.to_i - 1]
+  end
   #position method takes in user input of "1-9" strings, looks up value of cells at according index. All other methods take this input and use #position to look up value according to array index
   #update method represents updating board after first move. Two arguments: position user wants to occupy (1-9 string converted to array index), and player object making move. Also updates approrpiate index in cells, set equal to token of player object by calling #token method
   #full? 
