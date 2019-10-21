070c03b106dbd544f7320729787c6a894f86e307
Blythe Pollard <blytheepollard@gmail.com>
2019-10-21 01:41:51 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -26,9 +26,32 @@ class Board
     new_position = @cells[input.to_i - 1]
   end
   #position method takes in user input of "1-9" strings, looks up value of cells at according index. All other methods take this input and use #position to look up value according to array index
-  #update method represents updating board after first move. Two arguments: position user wants to occupy (1-9 string converted to array index), and player object making move. Also updates approrpiate index in cells, set equal to token of player object by calling #token method
-  #full? 
+  
+
+  def full?
+    @cells.all? do |position|
+    position == "X" || position == "O" 
+  end
+  end
+  
+  def turn_count
+    counter = 0 
+    @cells.each do |space| 
+      if space == "X" || space == "O" 
+      counter += 1 
+    end
+  end
+  counter
+  end
+
+  def taken?(token)
+    if @cells[token.to_i] == " " || @cells[token.to_i] == "" || @cells[token.to_i] == nil
+    false
+  else true
+    end
+  end
   #turn_count
   #taken?
   #valid_move?
+  #update method represents updating board after first move. Two arguments: position user wants to occupy (1-9 string converted to array index), and player object making move. Also updates approrpiate index in cells, set equal to token of player object by calling #token method
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,8 @@
 class Player
-  
+  attr_accessor :token
+  def initialize(token)
+    @token = token
+  end
   #not a valid player, but a root class that will act like inheritance point for actual player classes such as Human < Player and Computer < Player
   #defines most basic properties of a player: token that is set on initialization
   #all player subclasses implement #move method
