3c6fb9e24b909bb0ed8e159d96e712866d204a64
Blythe Pollard <blytheepollard@gmail.com>
2019-10-21 03:06:31 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -14,29 +14,27 @@ class Board
   #reset! method resets state of cells to blank board
   
   def display
-    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
     puts "-----------"
-    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
     puts "-----------"
-    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
   end
   #display method prints current state of board
   
   def position(input)
-    new_position = @cells[input.to_i - 1]
+    cells[input.to_i - 1]
   end
-  #position method takes in user input of "1-9" strings, looks up value of cells at according index. All other methods take this input and use #position to look up value according to array index
   
-
   def full?
-    @cells.all? do |position|
+    cells.all? do |position|
     position == "X" || position == "O" 
   end
   end
   
   def turn_count
     counter = 0 
-    @cells.each do |space| 
+    cells.each do |space| 
       if space == "X" || space == "O" 
       counter += 1 
     end
@@ -44,14 +42,16 @@ class Board
   counter
   end
 
-  def taken?(token)
-    if @cells[token.to_i] == " " || @cells[token.to_i] == "" || @cells[token.to_i] == nil
-    false
-  else true
-    end
+  def taken?(input)
+    !(position(input) == " " || position(input) == "" || position(input) == nil)
   end
-  #turn_count
-  #taken?
-  #valid_move?
-  #update method represents updating board after first move. Two arguments: position user wants to occupy (1-9 string converted to array index), and player object making move. Also updates approrpiate index in cells, set equal to token of player object by calling #token method
+  
+  def valid_move?(input)
+  input.to_i.between?(1,9) && !taken?(input)
+  end
+  
+  def update(input, player)
+    cells[input.to_i - 1] = player.token
+  end
+  
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,9 @@
 class Player
-  attr_accessor :token
+  
+  attr_reader :token
+  
   def initialize(token)
     @token = token
   end
-  #not a valid player, but a root class that will act like inheritance point for actual player classes such as Human < Player and Computer < Player
-  #defines most basic properties of a player: token that is set on initialization
-  #all player subclasses implement #move method
+  
 end
