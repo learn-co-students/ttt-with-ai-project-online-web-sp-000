155f099deeca7744097f44cac6fc72cad9731835
Blythe Pollard <blytheepollard@gmail.com>
2019-10-21 23:39:10 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,3 +1,4 @@
+require 'pry'
 #Players::Computer class represents a computer player of tic tac toe
 #move method accepts board and returns the move the computer wants to make in 1-9 String
     #returns a valid move 
@@ -7,9 +8,14 @@
 module Players
   class Computer < Player
     
-    def move(input)
-     @board.valid_move?(input)
-    end
+    def move(board)
+     move = nil
+     #if bottom left isnt taken, move, else take top left
+     #if top left isn't taken, move, else take bottom right
+     #if bottom right isn't taken, move, else take top right
+     #
+     
+  end
     
   end
 end
