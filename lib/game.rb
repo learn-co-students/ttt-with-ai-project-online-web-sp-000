b7588208baf72b27fd3f76d374d7194ba7d430c4
Blythe Pollard <blytheepollard@gmail.com>
2019-10-21 03:10:33 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,16 @@
 class Game
+  
+  WIN_COMBINATIONS = [
+     [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ]
+  
   #main model of app
   #represents a singular instance of tic tac toe session:
     #game has one board through board property
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,9 +1,14 @@
 #define Human class that inherits from Player
 #human player must implement #move method (that takes in board argument) which allows human player to enter a move in cli
 
-# module Players
-#   class Human < Player
-#     # your code here
-#   end
-# end
+module Players
+  class Human < Player
+    
+    def move(board)
+      puts "What is your move?"
+      input = gets.strip
+    end
+    
+  end
+end
 # Now when you call your Human class, you'll have to call it inside its name space with Players::Human.
