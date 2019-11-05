5b2c6f2a76663717ebfce3995951a2542a709ec6
Ronda D Cowen <cowenhome5@gmail.com>
2019-11-05 23:59:44 +0000

    Done.

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,3 +1,14 @@
+module Players
 
   class Human < Player
+
+    def initialize(board)
+      @board = board
+    end
+
+    def move(board = nil)
+      input = gets
+      input
+    end
   end
+end
