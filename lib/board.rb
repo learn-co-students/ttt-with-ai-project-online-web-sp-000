180c2fa8282ad24e691e9b11cf37992fba976543
Michael Carroll <carrollm2@gmail.com>
2019-11-22 03:08:30 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,26 @@
+class  Board
+
+  attr_accessor :cells
+
+  def initialize
+    @cells = Array.new(9," ")
+    @cells.map do |cell|
+      cell.to_s
+    end
+  end
+
+  def reset!
+    self.cells = Array.new(9," ")
+  end
+
+  def display
+
+    puts " #{self.cells[0]}" + " | " + "#{self.cells[1]}" + " | " + "#{self.cells[2]} "
+    puts "------------"
+    puts " #{self.cells[3]}" + " | " + "#{self.cells[4]}" + " | " + "#{self.cells[5]} "
+    puts "------------"
+    puts " #{self.cells[6]}" + " | " + "#{self.cells[7]}" + " | " + "#{self.cells[8]} "
+
+  end
+
+end
