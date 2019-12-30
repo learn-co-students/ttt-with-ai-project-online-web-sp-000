04d803c0a0f5c9882080f5f537951e85c6f7f57b
Jason J Cruz <jaycru06@yahoo.com>
2019-12-30 21:55:12 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -17,7 +17,13 @@ class Game
      [6, 4, 2]
    ]
 
-
+   def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"),
+                  board = Board.new)
+     # defaults to two human players
+     @board = board
+     @player_1 = player_1
+     @player_2 = player_2
+   end
 
 end
 
