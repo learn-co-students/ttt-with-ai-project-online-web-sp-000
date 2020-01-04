7d0b2923aacbe49eb5b39ccca528f2b173f98b1c
Jason J Cruz <jaycru06@yahoo.com>
2020-01-04 02:29:12 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,10 +2,16 @@ module Players
   class Computer < Player
 
     def move(board)
-      # returns a valid position for the computer to move
+      # Implement a #move method that accepts
+      # a board and returns the move the computer wants to make in the
+      # form of a 1-9 string. How the computer decides to make that
+      # move is up
+      # to you but it must be capable of returning a valid move at some
+      # point.
       # binding.pry
-      # move = nil
-      # board.cells.taken?(5)
+      if board.cells[4] == " "
+        return "5"
+      end
     end
 
   end
