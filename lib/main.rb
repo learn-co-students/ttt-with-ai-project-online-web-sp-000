50291c37cbcba0bc4d13b87ddae512e512866fc4
Will Carter <will.carter@fergusllc.com>
2020-01-18 21:50:31 -0700

    Done.

--- lib/main.rb
+++ lib/main.rb
@@ -1,17 +1,17 @@
-require_relative 'board.rb'
-require_relative 'game.rb'
-require_relative 'player.rb'
-require_relative 'players/computer.rb'
-require_relative 'players/human.rb'
+# require_relative 'board.rb'
+# require_relative 'game.rb'
+# require_relative 'player.rb'
+# require_relative 'players/computer.rb'
+# require_relative 'players/human.rb'
 
-h = Players::Human.new("O")
-c = Players::Computer.new("X")
-b = Board.new
-b.cells = [ " ", " ", " ", 
-            " ", " ", " ", 
-            " ", " ", " "]
-game = Game.new(h, c, b)
-game.play
+# h = Players::Human.new("O")
+# c = Players::Computer.new("X")
+# b = Board.new
+# b.cells = [ " ", " ", " ", 
+#             " ", " ", "O", 
+#             "X", "O", "X"]
+# game = Game.new(c, h, b)
+# game.play
 
 # c1 = Players::Computer.new("O")
 # c = Players::Computer.new("X")
