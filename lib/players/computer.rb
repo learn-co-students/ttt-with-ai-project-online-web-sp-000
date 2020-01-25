e8281c994fe64969ab758475d4f22f5014acbbc0
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-25 01:29:53 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -7,8 +7,18 @@ module Players
     # the move the computer wants to make in the form of a 1-9 string.
     # How the computer decides to make that move is up to you
     # but it must be capable of returning a valid move at some point.
+
+    # def x_o_count
+    #   x = @cells.count("X")
+    #   o = @cells.count("O")
+    # end
+
+    # if the computer goes first ("X") it should choose the middle
     def move(board)
-      turn_tracker = game.board.turn
+      x = @board.count("X")
+      o = @board.count("O")
+
+      turn_tracker = @board.x_o_count
         if self.token == "X"
           return "5"
         end
