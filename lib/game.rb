3feddf6a6545199373f92e81e86f2d7c0903b0aa
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-26 19:38:42 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,5 @@
+# require_relative 'bin/tictactoe.rb'
+
 class Game
 
   attr_accessor :board, :player_1, :player_2
@@ -85,14 +87,84 @@ class Game
     # after every turn, check this... since #over checks won and draw i can exclude them
     until over? do
       self.turn
+      @board.display
     end
     if self.won?
       puts "Congratulations #{self.winner}!"
     else self.draw?
       puts "Cat's Game!"
     end
-
   end
+
+  # game between 2 computers
+  def ai_game
+    @player_1 = Computer.new("X")
+    @player_1.board = self.board
+    @player_2 = Computer.new("O")
+    @player_2.board = self.board
+    self.play
+    self.play_again?
+  end
+
+  # game between human and computer
+  def human_v_computer
+    puts "Would you like to go first? (Y/N)"
+      input = gets.chomp
+      if input == "Y" || input == "y"
+        # @player_1 = Human.new("X")
+        @player_2 = Computer.new("O")
+        @player_2.board = self.board
+      elsif input == "N" || input == "n"
+        @player_1 = Computer.new("X")
+        @player_1.board = self.board
+      else
+        puts "Not a valid entry."
+        self.play
+      end
+  end
+
+  # game between 2 humans
+  def human_v_human
+    puts "Great, glad you have a friend!"
+    puts "Select a number between 1-9"
+    self.play
+  end
+
+  # When the game is over, the CLI should prompt the user if they would like to
+  # play again and allow them to choose a new configuration for the game as described above.
+  # If the user doesn't want to play again, exit the program.
+  def play_again?
+    answer = gets.chomp
+    if over?
+      puts "Play again? (Y/N)"
+      if answer == Y || answer == "y"
+        Board.reset!
+        self.begin_game
+      else
+        puts "Ok, see you later!"
+      end
+    end
+  end
+
+  # Greet the user with a message.
+  # Prompt the user for what kind of game they want to play, 0,1, or 2 player.
+  # Ask the user for who should go first and be "X".
+  # Use the input to correctly initialize a Game with the appropriate player types and token values.
+
+  def begin_game
+    puts "Hello there, welcome to Tic-tac-toe"
+    puts "Would you like to play with 0, 1, or 2 players?"
+    input = gets.chomp
+    input
+      if input == "0"
+        self.ai_game
+      elsif input == "1"
+        self.human_v_computer
+      else input == "2"
+        self.human_v_human
+      end
+  end
+
 
 # class end
 end
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,6 +2,7 @@ module Players
 # Every player subclass will implement a #move method
 # that represents how that type of player makes a move in Tic-tac-toe.
   class Computer < Player
+    attr_accessor :board
 
     # Implement a #move method that accepts a board and returns
     # the move the computer wants to make in the form of a 1-9 string.
@@ -16,22 +17,29 @@ module Players
     # inside #move, board = #<Board:0x0000000003364838 @cells=[" ", " ", " ", " ", " ", " ", " ", " ", " "]
     # inside #move, @board and @cells are nil
 
-    # Strategy ------
+    # ------ Strategy ------
     # if the computer goes first (token "X") it should choose the middle
     # if there's an O on the board, the computer should choose a place next to it
     # if the computer has 2 neighboring tokens on the board, it should choose the 3rd spot to win
     def move(board)
-      binding.pry
-      x = @cells.count("X")
-      o = @cells.count("O")
-
-      turn_tracker = @cells.x_o_count
+      # binding.pry
+      # x = @cells.count("X")
+      # o = @cells.count("O")
+      #
+      # turn_tracker = @cells.x_o_count
+        # if the computer goes first (token "X") it should choose the middle
         if self.token == "X"
           return "5"
+        # elsif self.token == "O"
+        #   return ""
         end
+
     end
 
+
+
+    # class end
   end
 
-
+  # module end
 end
