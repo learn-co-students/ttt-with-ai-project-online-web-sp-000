53c2653ad7755a2d63cd6de1f622a6bb898d67b0
cantstopcoding <jonathan_koss@yahoo.com>
2020-02-10 11:36:21 -0500

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,38 @@
 class Board 
     attr_accessor :cells
 
-    def reset!
+    def initialize
         @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     end
     
-    
+    def reset!
+        @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    end
+
+    def display
+        puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+        puts "-----------"
+        puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+        puts "-----------"
+        puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+    end
+
+    def position(input)
+        cells[input.to_i - 1]
+    end
+
+    def full?
+        cells.all?{|cell| cell != " "}
+    end
+
+    def turn_count
+        cells.count{|cell| cell != " "}
+    end
+
+    def taken?
+        # if the position is taken with and X or O then return true
+
+    end
 end 
 
 # board = Board.new
