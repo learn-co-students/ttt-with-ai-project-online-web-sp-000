4fca4c3c59c210b2f3a33003e27970cf30ba8743
cantstopcoding <jonathan_koss@yahoo.com>
2020-02-11 21:56:39 -0500

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -8,8 +8,25 @@ class Game
     [0, 4, 8], [2, 4, 6]
     ]
 
-    def initialize
+    def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+        @player_1 = player_1
+        @player_2 = player_2
+        @board = board
+    end
 
+    def current_player
+        board.turn_count.even? ? player_1 : player_2 
+    end
+
+    def won?
+        WIN_COMBINATIONS.find do |combo|
+            board.cells[combo[0]] == board.cells[combo[1]] && board.cells[combo[1]] == board.cells[combo[2]] && board.cells[combo[0]] != " "
+        end
+    end
+
+    def draw?
+        # board.cells.full? && won?
+        # binding.pry
     end
 
 end
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -2,7 +2,7 @@ module Players
     class Human < Player
         # Ask user for input and return it
         def move(board)
-            input = gets.strip.chomp
-        end
+            gets.strip.chomp
+        end                                       
     end
-end
+end 
