56459f8a94306e27c192d1dc503118cefbf265c3
Chloe Gell <gellert.chloe@gmail.com>
2020-02-13 01:16:06 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,32 @@
 class Game
-  attr_accessor :player_1, :player_2
-  def initialize(player1, player2)
-    @player_1 = player1 
-    @player_2 = player2
-    Board.new
+  attr_accessor :player_1, :player_2, :board
+  def initialize(player1=nil, player2=nil, board=nil)
+    if(player1)
+      @player_1 = player1
+    else 
+      @player_1 = Players::Human.new("X") 
+    end 
+    if(player2)
+      @player_2 = player2
+    else 
+      @player_2 = Players::Human.new("O")
+    end 
+    if(board)
+      @board = board 
+    else 
+      @board = Board.new 
+    end 
   end 
+  WIN_COMBINATIONS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ]
   def current_player 
-    Board.turn_count.even? ? "X" : "O"
+    @board.turn_count.even? ? "O" : "X"
+  end 
+  def board 
+    @board 
   end 
   def won? 
   end 
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -4,6 +4,9 @@ module Players
   class Human < Player 
     def move(board)
       #return user entered value
+      puts "Please enter the space you would like to take:"
+      input = gets.strip
+      input 
     end 
   end 
 end 
