ee8901bf6b2a3828e772ca2f6e7fdc53b203920b
Chloe Gell <gellert.chloe@gmail.com>
2020-02-13 00:30:02 +0000

    Done.

---
+++ lib/player.rb
@@ -0,0 +1,6 @@
+class Player 
+  attr_reader :token 
+  def intialize(token)
+    @token = token 
+  end 
+end 
---
+++ lib/players/computer.rb
@@ -0,0 +1,9 @@
+require_relative '../player.rb'
+
+module Players 
+  class Human < Player 
+    def move(board)
+      #return user entered value 
+    end 
+  end 
+end 
