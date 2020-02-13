71fd95769b551c11f4c206612daaa4cea42a5bb4
Chloe Gell <gellert.chloe@gmail.com>
2020-02-13 00:02:33 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,24 @@
+class Board
+  def initialize
+    reset! 
+  end 
+  def reset! 
+    @cells = [" ", " ", " ", " ", " ", " ", " "," ", " "]
+  end 
+  def display 
+  end 
+  def position(space)
+  end 
+  def update(position, token)
+  end 
+  def token(token)
+  end 
+  def full?
+  end 
+  def turn_count
+  end 
+  def taken?
+  end 
+  def valid_move?
+  end 
+end 
