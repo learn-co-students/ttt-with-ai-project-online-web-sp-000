8c743b3e9e3b49e52b42a9b5441cd978efaf57c4
Chloe Gell <gellert.chloe@gmail.com>
2020-02-13 00:07:14 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,5 @@
 class Board
+  attr_accessor :cells 
   def initialize
     reset! 
   end 
@@ -6,6 +7,11 @@ class Board
     @cells = [" ", " ", " ", " ", " ", " ", " "," ", " "]
   end 
   def display 
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
   end 
   def position(space)
   end 
