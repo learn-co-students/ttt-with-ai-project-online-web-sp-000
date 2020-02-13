7ff2768923c07f9fd7d24737229fd357f99dba05
Chloe Gell <gellert.chloe@gmail.com>
2020-02-13 23:58:06 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,10 +3,13 @@ require_relative '../player.rb'
 module Players 
   class Computer < Player 
     def move(board)
-      #return user entered value 
-      puts "Please enter the space you would like to take:"
-      input = gets.strip
-      input 
+      #if corners not taken claim corner 
+      #else claim middle 
+      #
     end 
   end 
 end 
+
+corners in idx = [0, 2, 6, 8]
+middle = 4 
+leftovers = 1, 3, 5, 7 
