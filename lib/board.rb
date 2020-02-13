270d66bcc3a62881c1e0f5976d46a5b3d084b55b
Chloe Gell <gellert.chloe@gmail.com>
2020-02-13 00:15:20 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -14,17 +14,75 @@ class Board
     puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
   end 
   def position(space)
+    space.to_i - 1
   end 
   def update(position, token)
+    
   end 
   def token(token)
   end 
   def full?
+    @cells.all? {|square| square != " "} 
   end 
   def turn_count
+    @cells.count{|square| square != " " }
   end 
-  def taken?
+  def taken?(idx)
+    @cells[idx] != " "
   end 
   def valid_move?
   end 
 end 
+
+  def move(index, token)
+    @board[index] = token
+  end
+
+  def valid_move?(index)
+    !position_taken?(index) && index.between?(0,8)
+  end
+
+  def current_player
+    turn_count.even? ? "X" : "O"
+  end
+
+  def turn
+    puts "Please enter a number (1-9):"
+    user_input = gets.strip
+    index = input_to_index(user_input)
+    if valid_move?(index)
+      token = current_player
+      move(index, token)
+    else
+      turn
+    end
+    display_board
+  end
+
+  def won?
+    WIN_COMBINATIONS.any? do |combo|
+      if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
+        return combo
+      end
+    end
+  end
+
+  def draw?
+    full? && !won?
+  end
+
+  def over?
+    won? || draw?
+  end
+
+  def winner
+    if combo = won?
+      @board[combo[0]]
+    end
+  end
+
+  def play
+    turn until over?
+    puts winner ? "Congratulations #{winner}!" : "Cat's Game!"
+  end
+end
