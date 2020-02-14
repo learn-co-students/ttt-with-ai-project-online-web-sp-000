ee8847690896026ab1ab8a12cbdc38526851ccc9
Chloe Gell <gellert.chloe@gmail.com>
2020-02-14 00:28:05 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -6,6 +6,9 @@ module Players
       #if corners not taken claim corner 
       #else claim middle 
       #
+    end
+    def valid?(idx)
+      @board(idx) != "" && @board(idx) != " "
     end 
   end 
 end 
@@ -13,3 +16,12 @@ end
 # corners in idx = [0, 2, 6, 8]
 # middle = 4 
 # leftovers = 1, 3, 5, 7 
+# def turn 
+#     current_move = current_player.move(@board)
+#     if @board.valid_move?(current_move)
+#       @board.update(current_move, current_player)
+#     else
+#       turn
+#     end
+#     @board.display
+#   end 
