1411b2021d13a362216aaffe057d1f7f3794d21b
github username <github email address>
2020-02-25 19:52:47 -0700

    displayed board

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -3,20 +3,10 @@ module Players
     class Human < Player
 
         def move(board)
+            board.display
             puts "Please choose a position by typing: 1-9"
             gets.strip
         end
 
     end
 end
-
-
-# Players::Human
-# Define a class Human that inherits from Player. This class should be namespaced inside the module Players because the human.rb file is inside the players/ directory
-
-# def move
-# end
-# method that takes in a board argument and allows a human player to enter a move via the CLI. The method should return the value the user enters. Even though the method accepts a board argument, it does not need to use it.
-
-#?? Now when you call your Human class, you'll have to call it inside its name space with 
-#Players::Human. ?? ( which file does this go in)
