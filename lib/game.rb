03b2efb3fabd42401856f532e40421470b3087dd
Amy Fong <amyhfong@gmail.com>
2020-02-28 19:55:04 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -21,10 +21,45 @@ class Game
     @player_2 = player_2
   end
 
-  def initialize(player_1 = "X", player_2 = "O", board = Board.new)
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
     @player_1 = player_1
     @player_2 = player_2
     @board = board
+  end
+
+  def current_player
+    self.board.turn_count.odd? ? @player_2 : @player_1
+  end
+
+  def won?
+    WIN_COMBINATIONS.find do |win_array|
+      win_array.all? {|i| self.board.cells[i] == "X"} || win_array.all? {|i| self.board.cells[i] == "O"}
+    end
+  end
+
+  def draw?
+    self.board.full? && !self.won?
+  end
+
+  def over?
+    self.draw? || self.won?
+  end
+
+  def winner
+    self.won? ? self.board.cells[won?[0]] : nil
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    input = self.current_player.gets
+    # self.board.valid_move?(input)
+    if self.board.valid_move?(input)
+      move(index, current_player)
+      display_board
+    else
+      turn
+    end
+
   end
 
 end
