f803e8a1a1ee16ca953106a8687acc0b8877bb5c
Kirsten Keels <keelsk@gmail.com>
2020-03-11 03:06:10 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,8 +2,14 @@ require 'pry'
 module Players
   class Computer < Player
     def move(board)
-      position = rand(1..9)
-      position.to_s
+      move = nil
+      if !(board.taken?(5))
+        move = "5"
+      else 
+        array = ["1", "2", "3", "4", "6", "7", "8", "9"]
+        move = array.sample
+      end
+      move
     end
   end
 end
