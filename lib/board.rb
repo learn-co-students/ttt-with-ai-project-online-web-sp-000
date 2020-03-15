4dc73fb8373ba16b2daa689eda561bf40c417daf
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-03-15 00:35:01 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Board
   attr_accessor :cells
 
@@ -12,19 +14,43 @@ class Board
 
   def display
     puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+    puts "-----------"
     puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
-    puts " #{cells[7]} | #{cells[8]} | #{cells[9]} "
+    puts "-----------"
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
   end
 
   def position(input)
-    input = gets.chomp.to_i
-    if @cells[input - 1] == " " ? true : false
-    end
+    cell = input.to_i - 1
+    self.cells[cell]
   end
 
   def update (position, object)
-    @cells[index - 1] = object if position == true
+    object = player.token
+    position = object if !self.taken?
   end
+
+  def full?
+    !@cells.include?(" ")
+  end
+
+  def turn_count
+    @cells.count {|v| v == "X" || v == "O"}
+  end
+
+  def taken?(input)
+    position(input) != " " ? true : false
+  end
+
+  def valid_move?(input)
+    index = input.to_i - 1
+    index.to_i.between?(0,8) && !taken?(input)
+ end
+
+ def update (input, player)
+   index = input.to_i - 1
+   @cells[index] = player.token
+ end
 
 
 
