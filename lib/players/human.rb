e517522c07b99b2a1ad83c0900aa8a0fab0c84a4
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-04-06 21:55:19 +0000

    Done.

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,9 +1,14 @@
 module Players
   class Human < Player
     def move(board)
-      puts "Please make a move:"
-      input = gets.strip
-      input
+      if board.turn_count == 0
+        board.display
+        puts "Please make a move:"
+        gets.strip
+      else
+        puts "Please make a move:"
+        gets.strip
+      end
     end
   end
 end
