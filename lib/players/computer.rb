2b1b6bda71be58571b6899e44f10a0fb36e54231
C. Leow <chienleow1@gmail.com>
2020-04-20 21:09:54 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,10 +1,16 @@
+require 'pry'
+
 module Players
   class Computer < Player
 
-    def move(board)
-
-
-    end
+    # def move(board)
+    #
+    # end
+    #
+    # def next_move(board)
+    #   # binding.pry
+    #   board.cells.each.with_index(1) {|cell| cell == " "}
+    # end
 
   end
 end
