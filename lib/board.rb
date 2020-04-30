fa380042a38c10e7e09092c9826019fb3af581d1
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-30 17:41:59 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,14 @@
 class Board
   
-  def cells
-    puts " #{board[0]} | #{board[1]} | #{board[2]} "
-    puts "-----------"
-    puts " #{board[3]} | #{board[4]} | #{board[5]} "
-    puts "-----------"
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  attr_accessor :cells
+  
+  def initialize
+    self.reset!
+  end
+  
+  def reset!
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
   
 end
+  
