64d3d879a9df943c77ea7c5d7ecc1e588587846a
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-30 21:44:30 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -10,5 +10,59 @@ class Board
     @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
   
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+  
+  def position(input)
+    input = input.to_i - 1
+    @cells[input]
+  end
+  
+  def full?
+    @cells.all? {|value| value != " "}
+  end
+  
+  def turn_count
+    counter = 0
+    @cells.each do |position|
+      if position == "X" || position == "O"
+        counter += 1
+      end
+    end
+    counter
+  end
+  
+  def taken?(input)
+    if position(input) == " " || position(input) == ""
+      false
+    elsif position(input) == "X" || position(input) == "O"
+      true
+    else
+      false
+    end
+  end
+  
+  def valid_move?(input)
+    if input !~ /\D/
+      if input.to_i.between?(0,8) && !taken?(input) 
+        true
+      else
+        false
+      end
+    else
+      false
+    end
+  end
+  
+  def update(input, player)
+    
+  end
+  
+  
 end
   
