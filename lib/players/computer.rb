b80e83171bea4cd2dba458302a560ec2509e4707
Anna Guerrero <acg2176@columbia.edu>
2020-05-17 22:14:10 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,21 +1,13 @@
 module Players
   class Computer < Player
-    # def move(board)
-    #   board.cells.each do |slot|
-    #     if slot[4] != " "
-    #       return "5"
-    #     else
-    #       return "9"
-    #     end
-    #   end
-    # end
     def move(board)
-    if board.position("5") == " " then "5"
-    else board.cells.each_with_index do |cell, index|
-      if cell == " " then index+1
+      board.cells.each do |slot|
+        if slot[4] != " "
+          return "5"
+        else
+          return "9"
+        end
       end
     end
-   end
-  end
   end
 end
