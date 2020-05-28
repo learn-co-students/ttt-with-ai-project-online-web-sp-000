6ea50c7e9774164266dd17ab11de000027272624
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-28 22:05:11 +0000

    saving

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,19 +1,15 @@
 require 'pry'
 module Players
   class Computer < Player
-    # SIDES = [2,6,8,4]
-    
-    # CORNERS = [1,3,7,9]
-    
     WIN_COMBINATIONS = [
-    [1,2,3], 
-    [4,5,6],
-    [7,8,9],
-    [1,4,7],
-    [2,5,8],
-    [3,6,9],
-    [1,5,9],
-    [3,5,7]
+      [1,2,3],
+      [4,5,6],
+      [7,8,9],
+      [1,4,7],
+      [2,5,8],
+      [3,6,9],
+      [1,5,9],
+      [3,5,7]
     ]
    
     OPPOSITE_CORNERS = {
@@ -22,34 +18,39 @@ module Players
       7 => 3,
       9 => 1
       }
-    
-    def move(board)
-      sleep(0.75)
-      # puts "can win opp: #{can_win_opponent?(board)}"
-      # puts "can win self: #{can_win_self?(board)}"
-      # puts "can center: #{can_take_center?(board)}"
-      # puts "can corner oppo: #{can_opposite_corner?(board).compact}"
-      # puts "can corner: #{can_corner?(board)}"
-      # puts "can side: #{side_available?(board)}"
       
+    # Move method contains logic 
+    # to determine the proper move
+    def move(board)
+      # 1 check for any one move wins
       if can_win_self?(board) != false
-        return can_win_self?(board)
+        return can_win_self?(board).to_s
+      
+      # 2 check for blocking one move wins
       elsif can_win_opponent?(board) != false
-        return can_win_opponent?(board)
+        return can_win_opponent?(board).to_s
+      
+      # 3 take center if open
       elsif can_take_center?(board) == true
-        return 5
+        return 5.to_s
+      
+      # 4 take corner opposite opponent corner
       elsif can_opposite_corner?(board).compact == true
-        return can_opposite_corner?(board)
+        return can_opposite_corner?(board).to_s
+      
+      # 5 take first empty corner
       elsif can_corner?(board)
-        return can_corner?(board)
+        return can_corner?(board).to_s
+      
+      # 6 take first empty side
       elsif side_available?(board) != false
-        return side_available?(board)
+        return side_available?(board).to_s
       end
     end
   
-  
     # =====================================
-    # Checks for any one move wins returns array of win combos
+    # Checks for any one move wins
+    # returns array of win combos
     def get_winning_combos(board)
       combos = WIN_COMBINATIONS.collect do |combo|
         if combo.count {|p| board.taken?(p) == true} == 2
@@ -59,36 +60,40 @@ module Players
       end
       combos.compact
     end
-    
     # =====================================
     # 1 check for any one move wins
-    # return false if none return position if exists
+    # return false if none return 
+    # position if exists
     def can_win_self?(board)
-      combos = get_winning_combos(board)
-      combos.each do |combo|
-        tokens = combo.collect {|p| board.position(p) if board.taken?(p)}
-        if tokens.compact[0] == self.token 
-          puts tokens.compact[0]
-          return combo.find {|p| board.taken?(p) == false}
+      get_winning_combos(board).each do |combo|
+        if combo.find {|p| board.taken?(p)} == self.token
+           return combo.find {|p| board.taken?(p) == false}
         end
       end
       false
     end
-        
     # =====================================
     # 2 check for blocking one move wins
     # return false if no opportunities return position if exists
     def can_win_opponent?(board)
       opponent_token = self.token == "X" ? "O" : "X"
-      combos = get_winning_combos(board)
-      combos.each do |combo|
-        tokens = combo.collect {|p| board.position(p) if board.taken?(p)}
-        if tokens.compact[0] == opponent_token
-          return combo.find {|p| board.taken?(p) == false}
+      get_winning_combos(board).each do |combo|
+        if combo.find {|p| board.taken?(p)} == opponent_token
+           return combo.find {|p| board.taken?(p) == false}
         end
       end
       false
     end
+      
+    #   combos = get_winning_combos(board)
+    #   combos.each do |combo|
+    #     tokens = combo.collect {|p| board.position(p) if board.taken?(p)}
+    #     if tokens.compact[0] == opponent_token
+    #       return combo.find {|p| board.taken?(p) == false}
+    #     end
+    #   end
+    #   false
+    # end
     
     # =====================================
     # 5 take center if open
@@ -131,12 +136,4 @@ end
 
 
 
-# 1 check for any one move wins
-# 2 check for blocking one move wins
-# 3 create fork
-# 4 block opponent fork
-# 5 take center if open
-# 6 take corner opposite opponent corner
-# 7 take an empty corner
-# 8 take empty side
 
