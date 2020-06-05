bdb553911210b7d94cc2cde8455dd60d5fb6d7db
Cody Alvarez <cody_alvarez19@outlook.com>
2020-06-05 02:23:01 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,30 @@
+class Board
+
+  attr_accessor :cells
+
+  def initialize
+    @cells = Array.new(9, " ")
+  end
+
+  def reset!
+    @cells.clear
+    @cells = Array.new(9, " ")
+  end
+
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def position(input)
+     user_input = input.to_i - 1
+     @cells[user_input]
+  end
+
+  def full?
+  end
+
+end
