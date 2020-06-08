eb059ec28fecfb52852ae96e5f801ea82858c9da
Arodriguez1030 <arodriguez1030@gmail.com>
2020-06-07 22:56:42 -0400

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -37,6 +37,7 @@ class Board
         !taken?(input) && input.to_i.between?(1,9)
     end
     
-    def update (input)
+    def update (input, player)
+        @cells[input.to_i - 1] = player.token
     end
 end
---
+++ lib/players/human.rb
@@ -0,0 +1,8 @@
+module Players
+    class Human < Player
+        def move(input)
+            puts "Please enter your selection."
+            input = gets.strip
+        end
+    end
+end
