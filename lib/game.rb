0f90b99c80ebb932212d20484a8c92a8e788bf33
Evan Burton <evan@heyevan.com>
2020-06-10 23:01:28 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -24,11 +24,11 @@ class Game
   end
 
   def won?
-    WIN_COMBINATIONS.each do |combination|   #[0,1,2] which is board range (0-8)
+    WIN_COMBINATIONS.each do |combination|
         if @board.cells[combination[0]] == @board.cells[combination[1]] &&
           @board.cells[combination[1]] == @board.cells[combination[2]] &&
           @board.taken?(combination[0]+1)
-          #Need to +1, because #taken? rspec test is working off user_input range (1-9)
+
           return combination
         end
       end
@@ -47,7 +47,7 @@ class Game
   def winner
     if won?
       combination = won?
-      @board.cells[combination[0]] 
+      @board.cells[combination[0]]
     end
   end
 
