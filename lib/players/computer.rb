cc1bcb822c0b224f5bef6621f10602682153d786
Arodriguez1030 <arodriguez1030@gmail.com>
2020-06-10 20:38:28 -0400

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,8 +2,12 @@ module Players
     class Computer < Player
 
         def move(computer_move)
-            counter = 1
-            valid_move = computer_move.cells
+            input = (1 + rand(9)).to_s
+            if computer_move.valid_move?(input) == false
+                self.move
+            else
+                return input
+            end
         end
     end
 end
