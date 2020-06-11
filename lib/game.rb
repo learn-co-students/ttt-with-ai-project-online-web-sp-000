8f1a7e2e3b496a2d9841d3697c30f48beef9e972
Cody Alvarez <cody_alvarez19@outlook.com>
2020-06-11 02:23:27 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -50,6 +50,7 @@ class Game
 
   def turn
     puts "Please enter a number 1-9:"
+    @board.display
     input = current_player.move(@board)
     if @board.valid_move?(input)
       @board.update(input, current_player)
