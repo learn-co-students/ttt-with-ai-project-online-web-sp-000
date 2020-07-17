e50b5f68b6a527c0b99d1e26dac2b55763b347e8
Seth Near <seth.j.near@gmail.com>
2020-07-17 23:13:57 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -10,21 +10,37 @@ module Players
         them = game.player_1
       end
 
-      case
+      self_positions = game.board.cells.each_index.select{ |i| game.board.cells[i] == self.token}
+      them_positions = game.board.cells.each_index.select{ |i| game.board.cells[i] == them.token}
 
 
-      index = game.board.cells.find_index{|cell| cell == " "}
-      input = (index + 1).to_s
+      case game.board.turn_count
+      when 0
+        input = "1"
+      when 1
+        them_positions == [5] ? input = "1" : input = "5"
+      when 2
+
+
+      when (3..9)
+        index = game.board.cells.find_index{|cell| cell == " "}
+        input = (index + 1).to_s
+      end
       return input
     end
 
 
   def winning_move?(player, index)
     pos = (index + 1).to_s
-    this_game.board.taken?(pos) ? return false : return true
+    if this_game.board.taken?(pos)
+      return true
+    else
+      return false
+    end
     this_game.board.cells[index] = player.token
     outcome = this_game.won?
     this_game.board.cells[index] = " "
     return outcome
   end
+end
 end
