abaae621df2830dde79fba38eb96b4f6a966f09b
Seth Near <seth.j.near@gmail.com>
2020-07-20 04:19:12 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,36 +4,23 @@ module Players
   class Computer < Player
 
     def move
-      if self == game.player_1
-        them = game.player_2
-      else
-        them = game.player_1
-      end
+      #Identify Players
+      self == game.player_1 ? them = game.player_2 : them = game.player_1
 
+      #Get/update lists of relevant positions before executing each move
       $self_positions = game.board.cells.each_index.select{ |i| game.board.cells[i] == self.token}.map!{|i| i + 1}.to_set
-
       $them_positions = game.board.cells.each_index.select{ |i| game.board.cells[i] == them.token}.map!{|i| i + 1}.to_set
-
       $empty_positions = (1..9).to_set - $self_positions - $them_positions
 
-
-
+      #Computer/AI strategy
       case game.board.turn_count
+      #First 4 turns are hardcoded
       when 0
-        if them.class == Players::Computer
-        input = "1"
-      else
-        input = "5"
-      end
-
+        input = "1" #Corner opening move gives opponent least number of positions that can end in win/draw
       when 1
-        if $them_positions.include?(game.board.center)
-          input = "1"
-        else
-          input = "5"
-        end
+        $them_positions.include?(game.board.center) ? input = "1" : input = "5"  # Respond to center opening move with a corner, or take the center if available
       when 2
-        if game.board.taken?("5")
+        #Play corner opposite original
           if game.board.taken?("1")
             input = "9"
           elsif game.board.taken?("9")
@@ -42,24 +29,17 @@ module Players
             input = "7"
           elsif game.board.taken?("7")
             input = "3"
-          else
-            index = game.board.cells.find_index{|cell| cell == " "}
-            input = (index + 1).to_s
           end
-        end
-      when 3
-        empty_edges = (game.board.edges - $them_positions - $self_positions).to_a
-        input = empty_edges[0].to_s
-      when (4..9)
-
-        if winning_move?(self).count > 0
-          input = (winning_move?(self)[0] + 1).to_s
-        elsif winning_move?(them).count > 0
-          input = (winning_move?(them)[0] + 1).to_s
-        elsif fork?(self) != nil
-          input = (fork?(self) + 1).to_s
-        elsif fork?(them) != nil
-          input = (fork?(them) + 1).to_s
+      #Remaining turns use first available move from the list of "rules" (see comment at end of document)
+      when (3..9)
+        if two_in_a_row?(self).count > 0
+          input = (two_in_a_row?(self)[0] + 1).to_s
+        elsif two_in_a_row?(them).count > 0
+          input = (two_in_a_row?(them)[0] + 1).to_s
+        elsif fork_possible?(self) != nil
+          input = (fork_possible?(self) + 1).to_s
+        elsif fork_possible?(them) != nil
+          input = (fork_possible?(them) + 1).to_s
         elsif !game.board.taken?((game.board.center).to_s)
           input = (game.board.center).to_s
         elsif $them_positions.include?(1) && $empty_positions.include?(9)
@@ -75,152 +55,60 @@ module Players
         elsif ($empty_positions.to_set & game.board.edges).count > 0
           input = (($empty_positions.to_set & game.board.edges).to_a[0]).to_s
         else
-        index = game.board.cells.find_index{|cell| cell == " "}
-        input = (index + 1).to_s
-      end
-
-    # when 5
-    #   if winning_move?(self) != nil
-    #     input = (winning_move?(self) + 1).to_s
-    #   elsif winning_move?(them) != nil
-    #     input = (winning_move?(them) + 1).to_s
-    #   else
-    #   index = game.board.cells.find_index{|cell| cell == " "}
-    #   input = (index + 1).to_s
-    # end
-    # binding.pry
-  # when (4..9)
-  #   index = game.board.cells.find_index{|cell| cell == " "}
-  #   input = (index + 1).to_s
-
+          index = game.board.cells.find_index{|cell| cell == " "}   #Catch-all, *should* never have to be used
+          input = (index + 1).to_s
+        end
       end
       return input
     end
 
-    #
-    # if (game.current_player == self) && (winning_move?(self) != nil)
-    #   input = (winning_move?(self)).to_s
-    # elsif (game.current_player == self) && (winning_move?(them) != nil)
-    #   input = (winning_move?(them)).to_s
-    # else
-    # index = game.board.cells.find_index{|cell| cell == " "}
-    # input = (index + 1).to_s
-    # end
-def identify
-  game = game
-  if self == game.player_1
-    them = game.player_2
-  else
-    them = game.player_1
+    #Test for two in a rows (win or block)
+    def two_in_a_row?(player)
+      winning_indexes = []
+      existing_indexes = game.board.cells.each_index.select{ |i| game.board.cells[i] == player.token}
+      empty_indexes = $empty_positions.to_a.map!{|i| i - 1}
+      empty_indexes.each{|test_index|
+                          existing_indexes << test_index
+                          test_combos = existing_indexes.permutation(3).to_a
+                          existing_indexes.pop
+                          winning_combos = []
+                          Game::WIN_COMBINATIONS.each{ |win_combo|
+                            test_combos.include?(win_combo) ? winning_combos << win_combo : false}
+                          winning_combos.count > 0 ? winning_indexes << test_index : false
+                          }
+
+      return winning_indexes
+    end
+
+    #Make a fork if possible, or block opponent's potential fork (anticipates next move)
+    #Tries every empty position, and checks to see if (2) two_in_a_row's have been created as a result
+    def fork_possible?(player)
+      fork_index = nil
+      existing_indexes = game.board.cells.each_index.select{ |i| game.board.cells[i] == player.token}
+      empty_indexes = $empty_positions.to_a.map!{|i| i - 1}
+      empty_indexes.detect{|test_index|
+                            existing_indexes << test_index
+                            winning_indexes = two_in_a_row?(player)
+                            existing_indexes.pop
+                            if winning_indexes.count >= 2
+                              fork_index = test_index
+                              return true
+                            end
+                          }
+      return fork_index
+    end
+
   end
 end
 
-  def winning_move?(player)
 
-    winning_indexes = []
-    existing_indexes = game.board.cells.each_index.select{ |i| game.board.cells[i] == player.token}
+# AI Strategy/Rule list (use first available move from the list):
 
-    empty_indexes = $empty_positions.to_a.map!{|i| i - 1}
-
-    empty_indexes.each{|test_index|
-              # game.board.cells[test_index] = player.token
-              # winning_combo = game.won?
-              # game.board.cells[test_index] = " "
-              existing_indexes << test_index
-              # binding.pry
-              test_combos = existing_indexes.permutation(3).to_a
-
-              existing_indexes.pop
-              winning_combos = []
-              Game::WIN_COMBINATIONS.each{ |win_combo|
-                test_combos.include?(win_combo) ? winning_combos << win_combo : false
-                  }
-
-              # winning_combo = game.won?
-
-              if winning_combos.count > 0
-                winning_indexes << test_index
-              end
-             }
-
-    return winning_indexes
-end
-
-def fork?(player)
-  fork_index = nil
-  existing_indexes = game.board.cells.each_index.select{ |i| game.board.cells[i] == player.token}
-
-  empty_indexes = $empty_positions.to_a.map!{|i| i - 1}
-
-  empty_indexes.detect{|test_index|
-            # game.board.cells[test_index] = player.token
-            # winning_combo = game.won?
-            # game.board.cells[test_index] = " "
-            existing_indexes << test_index
-
-            # test_combos = existing_indexes.permutation(3).to_a
-
-
-
-            winning_indexes = winning_move?(player)
-
-            existing_indexes.pop
-
-            # winning_combo = Game::WIN_COMBINATIONS.detect{ |win_combo| test_combos.include?(win_combo)}
-
-            if winning_indexes.count >= 2
-              fork_index = test_index
-              return true
-            end
-            }
-
-            return fork_index
-            end
-
-
-
-
-
-
-
-
-# forks = Game::WIN_COMBINATIONS.select{|combo|
-#   test_index = (combo.to_set - empty_indexes.to_set).to_a
-#   if test_index.size ==
-#
-#
-# }
-#   if matches.count == 0
-#    return false
-#    else
-#    return matches
-#    end
-#   end
-
-end
-end
-
-
-
-#   experms = exes.permutation(3).to_a
-#   ohperms = ohs.permutation(3).to_a
-#   x_winning_index = WIN_COMBINATIONS.detect{ |win_combo| experms.include?(win_combo) }
-#   o_winning_index = WIN_COMBINATIONS.detect{ |win_combo| ohperms.include?(win_combo) }
-#  winning_pos = nil
-#  $empty_positions.to_a.find{}
-#   pos = (index + 1).to_s
-#   if game.board.taken?(pos)
-#     return true
-#   else
-#     return false
-#   end
-#   game.board.cells[index] = player.token
-#   outcome = game.won?
-#   game.board.cells[index] = " "
-#   return outcome
-
-# def empty_positions
-# $empty_positions = (1..9).to_set - $self_positions - $them_positions
-# return $empty_positions
-#   # return game.board.cells.select{ |cell| game.board.taken?(cell) }.to_set
-# end
+  # 1.  Win (two_in_a_row)
+  # 2.  Block (two_in_a_row)
+  # 3.  Create a Fork
+  # 4.  Block a Fork from being created
+  # 5.  Play the center position
+  # 6.  Play corner opposite corner taken previously by opponent.
+  # 7.  Play an available corner
+  # 8.  Play an available side/edge (middle position)
