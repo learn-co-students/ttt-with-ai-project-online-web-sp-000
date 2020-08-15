e2a9e720e179424c22cac29606afad7270d168b1
mirthful-concept-6030 <jchristinem@gmail.com>
2020-08-15 22:54:32 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -7,15 +7,40 @@ class Board
   end
 
   def reset!
-    @cells.concat([""] * 9)
+    @cells.clear.concat([" "] * 9)
   end
 
   def display
-    puts " #{@cells[0]}  |  #{@cells[1]}  |  #{@cells[2]} "
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
     puts "-----------"
-    puts " #{@cells[3]}  |  #{@cells[4]}  |  #{@cells[5]} "
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
     puts "-----------"
-    puts " #{@cells[6]}  |  #{@cells[7]}  |  #{@cells[8]} "
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def position(input)
+    index = input.to_i - 1
+    @cells[index]
+  end
+
+  def full?
+    @cells.all?{|cell| cell == "X" || cell == "O"}
+  end
+
+  def turn_count
+    @cells.count{|cell| cell == "X" || cell == "O"}
+  end
+
+  def taken?(input)
+    position(input) == "X" || position(input) == "O"
+  end
+
+  def valid_move?(input)
+    input.to_i.between?(1,9) && !taken?(input)
+  end
+
+  def update(input,player)
+    @cells[input.to_i - 1] = player.token
   end
 
 end
