37ce4912a0d51abe56db03581a3f685f10736602
mirthful-concept-6030 <jchristinem@gmail.com>
2020-08-16 04:35:04 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -44,7 +44,7 @@ class Game
 
   def turn
     input = current_player.move(@board)
-    @board.valid_move?(input) ? @board.update(input,current_player) : current_player.move(@board)
+    @board.valid_move?(input) ? @board.update(input,current_player) : turn
     @board.display
   end
 
