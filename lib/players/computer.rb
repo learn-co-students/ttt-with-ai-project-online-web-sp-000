21daa2736ee519f05777f74eee4317fff86a08e9
mirthful-concept-6030 <jchristinem@gmail.com>
2020-08-20 22:59:08 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -17,13 +17,15 @@ module Players
 
     def move(board)
       puts "Computer is thinking of a move..."
-      case board.turn_count
-      when <= 3
+      i = board.turn_count
+      case i
+      when 0..3
         input = [1,2,3,4,5,6,7,8,9].sample
-      when board.turn_count >= 3
-          case self.token
-          when "X"
-          offense_array = WIN_COMBINATIONS.collect{|combo| board.cells[combo[0]] == "X" && board.cells[combo[1]] == "X" || board.cells[combo[1]] == "X" && board.cells[combo[2]] == "X" || board.cells[combo[0]] == "X" && board.cells[combo[2]] == "X"}
+      when board.turn_count > 3
+        offense_array = offense_play?
+          if offense_play?
+            input = offense_array
+
           defense_array = WIN_COMBINATIONS.collect{|combo| board.cells[combo[0]] == "O" && board.cells[combo[1]] == "O" || board.cells[combo[1]] == "O" && board.cells[combo[2]] == "O" || board.cells[combo[0]] == "O" && board.cells[combo[2]] == "O"}
           if offense_array != nil
             win_move = offense_array.select{|index| index == " "}
@@ -32,9 +34,8 @@ module Players
             else
               defend_move = defense_array.select{|index| index == " "}
               input = defend_move.to_i + 1
-          next_move = move_array.select{|index| index == " "}
-            if next_move != nil
-              input = next_move.to_i + 1
+            end
+          end
             when "O"
             offense_array = WIN_COMBINATIONS.collect{|combo| board.cells[combo[0]] == "O" && board.cells[combo[1]] == "O" || board.cells[combo[1]] == "O" && board.cells[combo[2]] == "O" || board.cells[combo[0]] == "O" && board.cells[combo[2]] == "O"}
             defense_array = WIN_COMBINATIONS.collect{|combo| board.cells[combo[0]] == "X" && board.cells[combo[1]] == "X" || board.cells[combo[1]] == "X" && board.cells[combo[2]] == "X" || board.cells[combo[0]] == "X" && board.cells[combo[2]] == "X"}
@@ -45,12 +46,18 @@ module Players
               else
                 defend_move = defense_array.select{|index| index == " "}
                 input = defend_move.to_i + 1
-            next_move = move_array.select{|index| index == " "}
-              if next_move != nil
-                input = next_move.to_i + 1
-        end
-      end
+              end
+            end
+          end
     input.to_s
-    end
+  end
+end
+end
+end
+
+def offense_play?
+  move_array = WIN_COMBINATIONS.collect{|combo| board.cells[combo[0]] == "#{self.token}" && board.cells[combo[1]] == "#{self.token}" || board.cells[combo[1]] == "#{self.token}" && board.cells[combo[2]] == "#{self.token}" || board.cells[combo[0]] == "#{self.token}" && board.cells[combo[2]] == "#{self.token}"}
+  if move_array != nil
+    move_array.detect{|index| index == " "}
   end
 end
