95707f6c2b058b02904fa6ed7207610a1f4df87a
mirthful-concept-6030 <jchristinem@gmail.com>
2020-08-26 16:46:20 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -34,22 +34,75 @@ module Players
     ]
 
 
+def defense_play?
+  defense_plays = []
+  WIN_COMBINATIONS.each do |combo|
+      token_count = 0
+      opponent_token_count = 0
+      blank_count = 0
+    combo.each do |index|
+      if @board.cells[index] == @token
+        token_count += 1
+      elsif @board.cells[index] == @opponent_token
+        opponent_token_count += 1
+      else
+        blank_count += 1
+      end
+    end
+    if opponent_token_count == 2 && blank_count == 0
+      defense_plays << combo
+    end
+  end
+  defense_plays
+end
 
-
-
-def defense_play?(board)
-  possible_plays = []
-  possible_plays << WIN_COMBINATIONS.detect{|combo| board.cells[combo[0]] == "#{@opponent_token}" && board.cells[combo[1]] == "#{@opponent_token}"}
-  possible_plays << WIN_COMBINATIONS.detect{|combo| board.cells[combo[1]] == "#{@opponent_token}" && board.cells[combo[2]] == "#{@opponent_token}"}
-  possible_plays << WIN_COMBINATIONS.detect{|combo| board.cells[combo[0]] == "#{@opponent_token}" && board.cells[combo[2]] == "#{@opponent_token}"}
-  possible_plays.detect{|play| play != nil}
+def offense_play?
+  offense_plays = []
+  WIN_COMBINATIONS.each do |combo|
+      token_count = 0
+      opponent_token_count = 0
+      blank_count = 0
+    combo.each do |index|
+      if @board.cells[index] == @token
+        token_count += 1
+      elsif @board.cells[index] == @opponent_token
+        opponent_token_count += 1
+      else
+        blank_count += 1
+      end
+    end
+    if token_count == 2 && blank_count == 0
+      offense_plays << combo
+    end
+  end
+  offense_plays
 end
 
 
 
-def defense_move(board)
-  if defense_play?(board) != nil
-    defense_play?(board).detect{|index| board.cells[index] == " "}
+
+
+#def defense_play?(board)
+#  possible_plays = []
+#  possible_plays << WIN_COMBINATIONS.detect{|combo| board.cells[combo[0]] == "#{@opponent_token}" && board.cells[combo[1]] == "#{@opponent_token}"}
+#  possible_plays << WIN_COMBINATIONS.detect{|combo| board.cells[combo[1]] == "#{@opponent_token}" && board.cells[combo[2]] == "#{@opponent_token}"}
+#  possible_plays << WIN_COMBINATIONS.detect{|combo| board.cells[combo[0]] == "#{@opponent_token}" && board.cells[combo[2]] == "#{@opponent_token}"}
+#  possible_plays.detect{|play| play != nil}
+#end
+
+
+
+def defense_move
+  if defense_play?[0] != nil
+    defense_play?.sample.detect{|index| @board.cells[index] == " "}
+  else
+    nil
+  end
+end
+
+def offense_move
+  if offense_play?[0] != nil
+    offense_play?.sample.detect{|index| @board.cells[index] == " "}
   else
     nil
   end
@@ -64,30 +117,30 @@ def my_board
 end
 
 
-def offense_play?(board)
-  possible_plays = []
-  possible_plays << WIN_COMBINATIONS.collect{|combo| board.cells[combo[0]] == "#{@token}" && board.cells[combo[1]] == "#{@token}"}
-  possible_plays << WIN_COMBINATIONS.collect{|combo| board.cells[combo[1]] == "#{@token}" && board.cells[combo[2]] == "#{@token}"}
-  possible_plays << WIN_COMBINATIONS.collect{|combo| board.cells[combo[0]] == "#{@token}" && board.cells[combo[2]] == "#{@token}"}
-  #possible_plays.select{|play| play != nil}
-end
+#def offense_play?(board)
+#  possible_plays = []
+#  possible_plays << WIN_COMBINATIONS.collect{|combo| board.cells[combo[0]] == "#{@token}" && board.cells[combo[1]] == "#{@token}"}
+#  possible_plays << WIN_COMBINATIONS.collect{|combo| board.cells[combo[1]] == "#{@token}" && board.cells[combo[2]] == "#{@token}"}
+#  possible_plays << WIN_COMBINATIONS.collect{|combo| board.cells[combo[0]] == "#{@token}" && board.cells[combo[2]] == "#{@token}"}
+#  #possible_plays.select{|play| play != nil}
+#end
 
 
-def win_move(board)
-  win_move = []
-  if offense_play?(board)[0] != nil
-    offense_play?(board).each do |combo|
-      combo.each do |index|
-        if board.open_cells.include?(index + 1)
-          win_move << index
-        end
-      end
-    end
-    win_move.uniq.sample
-else
-nil
-end
-end
+#def win_move
+#  win_move = []
+#  if offense_play?(board)[0] != nil
+#    offense_play?(board).each do |combo|
+#      combo.each do |index|
+#        if board.open_cells.include?(index + 1)
+#          win_move << index
+#        end
+#      end
+#    end
+#    win_move.uniq.sample
+#else
+#nil
+#end
+#end
 
 def next_to(input, char)
   case input
@@ -133,33 +186,16 @@ def move
     input = @board.open_cells.select{|x| next_to(@game.last_move, board, " ").include?(x-1) && next_to(my_last_move, board, " ").include?(x-1)}.sample
     input -= 1
   when 3..7
-  #  puts "win move#{win_move(board)}"
-  #  puts "offense play #{offense_play?(board)}"
-  #  puts "defense move#{defense_move(board)}"
-  #  puts "defense play#{defense_play?(board)}"
-
-  #  puts "#{win_move(board)}"
-  #  if win_move(board) != nil
-      #  input = win_move(board)
-  #  elsif win_move(board) == nil && defense_move(board) != nil
-      #  input = defense_move(board)
-    #else
-
-    puts "win move#{win_move(board)}"
-    puts "defense move#{defense_move(board)}"
-
-    puts "#{win_move(board)}"
-    if win_move(board) != nil
-        input = win_move(board)
-    elsif defense_move(board) != nil
-        input = defense_move(board)
+      if win_move != nil
+        input = win_move
+    elsif win_move == nil && defense_move != nil
+        input = defense_move
     else
-
       defend_list = []
       WIN_COMBINATIONS.each do |combo|
         combo.each do |index|
-        if opponent_board(board).include?(index)
-          if combo.none?{|index| my_board(board).include?(index)}
+        if opponent_board.include?(index)
+          if combo.none?{|index| my_board.include?(index)}
             defend_list << combo
         end
         end
@@ -168,29 +204,29 @@ def move
     offense_list = []
     WIN_COMBINATIONS.each do |combo|
       combo.each do |index|
-      if my_board(board).include?(index)
-        if combo.none?{|index| opponent_board(board).include?(index)}
+      if my_board.include?(index)
+        if combo.none?{|index| opponent_board.include?(index)}
           offense_list << combo
       end
       end
     end
   end
-      play_1 = defend_list.flatten.detect{|index| defend_list.flatten.count(index) > 1 && board.open_cells.include?(index + 1)}
+      play_1 = defend_list.flatten.detect{|index| defend_list.flatten.count(index) > 1 && @board.open_cells.include?(index + 1)}
       possible_plays = []
-      possible_plays << WIN_COMBINATIONS.detect{|combo| board.cells[combo[0]] == " " && board.cells[combo[1]] == " "}
-      possible_plays << WIN_COMBINATIONS.detect{|combo| board.cells[combo[1]] == " " && board.cells[combo[2]] == " "}
-      possible_plays << WIN_COMBINATIONS.detect{|combo| board.cells[combo[0]] == " " && board.cells[combo[2]] == " "}
+      possible_plays << WIN_COMBINATIONS.detect{|combo| @board.cells[combo[0]] == " " && @board.cells[combo[1]] == " "}
+      possible_plays << WIN_COMBINATIONS.detect{|combo| @board.cells[combo[1]] == " " && @board.cells[combo[2]] == " "}
+      possible_plays << WIN_COMBINATIONS.detect{|combo| @board.cells[combo[0]] == " " && @board.cells[combo[2]] == " "}
       puts "#{possible_plays.uniq.flatten}"
       possible_plays = possible_plays.uniq.flatten
       play_2 = possible_plays.detect{|index| possible_plays.count(index) > 1}
 
-      if play_1 != nil && board.open_cells.include?(play_1 + 1)
+      if play_1 != nil && @board.open_cells.include?(play_1 + 1)
         input = play_1
       elsif
         play_2 != nil
         input = play_2
       else
-        input = board.open_cells.sample - 1
+        input = @board.open_cells.sample - 1
       end
 
       puts "#{possible_plays}"
