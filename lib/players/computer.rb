e6ad0e881dc44fab9d73d5a0df076a0f608abe91
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-09-11 00:03:53 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,15 +2,23 @@ module Players
   class Computer < Player
     
     def move(board)
+      # I basically tell the AI to pick the middle if it can
+      # If it's taken already AI goes for a random pick from odd position numbers (corners)
+      # Last resort is a random pick from even positions (everything besides the middle and the corners)
+      
       return "5" if !board.taken?("5")
-      return "1" if !board.taken?("1")
-      return "3" if !board.taken?("3")
-      return "7" if !board.taken?("7")
-      return "9" if !board.taken?("9")
-      return "2" if !board.taken?("2")
-      return "4" if !board.taken?("4")
-      return "6" if !board.taken?("6")
-      return "8" if !board.taken?("8")
+      
+      av_corners = ["1", "3", "7", "9"].select do |num|
+        !board.taken?(num)
+      end
+      
+      return av_corners.sample if !av_corners.empty?
+      
+      av_rest = ["2", "4", "6", "8"].select do |num|
+        !board.taken?(num)
+      end
+      
+      return av_rest.sample if !av_rest.empty?
     end
     
   end
