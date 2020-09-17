752ab76857940e18089deb5633f79ff9116c12ab
Adi Skop <adiskop@gmail.com>
2020-09-17 09:29:07 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,52 @@
+class Board
+
+attr_accessor :cells
+
+def initialize
+@cells=Array.new(9, " ")
+end
+
+def reset!
+  @cells.clear
+  @cells=Array.new(9, " ")
+end
+
+def display
+   puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+   puts "-----------"
+   puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+   puts "-----------"
+   puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+ end
+
+def position(input)
+puts "please enter your position: "
+cells[input.to_i-1]
+end
+
+def full?
+cells.all? {|token| token=="X" || token == "O"}
+end
+
+def turn_count
+  cells.count {|token| token=="X" || token == "O" }
+end
+
+def taken?(input)
+!(position(input)== " " || position(input) == "")
+end
+
+def valid_move?(input)
+  input.to_i.between?(1,9) && !taken?(input)
+
+end
+
+def update(input,player)
+  cells[input.to_i-1] = player.token
+end
+
+
+
+end
+# learn spec/01_board_spec.rb
+#learn spec/02_player_spec.rb
---
+++ lib/player.rb
@@ -0,0 +1,29 @@
+class Player
+
+attr_reader :token
+
+def initialize(token)
+  @token=token
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
+
+#learn spec/02_player_spec.rb
+
+#learn spec/03_human_player_spec.rb
+
+#learn spec/04_game_spec.rb
