726012a4b2f17e508ce3e07b59849ce4c49a15c3
Heath Mast <hrmast@gmail.com>
2020-11-05 20:31:13 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,6 +2,7 @@ module Players
   class Computer < Player
 
   def move(board)
+<<<<<<< HEAD
       if board.cells[4] == " "
         return "5"
       elsif board.cells[0] == " "
@@ -23,4 +24,29 @@ module Players
       end
     end
   end
+=======
+    if board.cells[4] == " "
+      "5"
+    elsif board.cells[0] == " "
+      "1"
+    elsif board.cells[2] == " "
+      "3"
+    elsif board.cells[6] == " "
+      "7"
+    elsif board.cells[8] == " "
+      "9"
+    elsif board.cells[1] == " "
+      "2"
+    elsif board.cells[3] == " "
+      "4"
+    elsif board.cells[5] == " "
+      "6"
+    elsif board.cells[7] == " "
+      "8"
+    end
+
+  end
+
+end
+>>>>>>> 97d26f2184d839c8074dfba8176ce5c6433fd5a8
 end
