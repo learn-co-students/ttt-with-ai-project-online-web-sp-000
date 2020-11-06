5463822ce482c53cd330e2d706e1b40742aa8675
Christerle Nounoute <christerlesn@gmail.com>
2020-11-06 04:33:25 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,23 +1,23 @@
-require 'pry'
 module Players
   class Computer < Player
-  #  binding.pry
-  #  @valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
     def move(board)
       move = nil
+
       if !board.taken?(5)
         move = "5"
-      elsif board.turn_count == 1 || board.turn_count == 2
-        move = [1,3,7,9].detect{|i| !board.taken?(i)}.to_s
-      elsif board.turn_count == 3 &&
-        board.position(1) == board.position(9) ||
-        board.position(3) == board.position(7)
+      elsif !board.taken?(1)
+        move = "1"
+      elsif !board.taken?(3)
+        move = "3"
+      elsif !board.taken?(7)
+        move = "7"
+      elsif !board.taken?(9)
+        move = "9"
+      elsif board.turn_count >= 3 && (board.position(1) == board.position(9) || board.position(3) == board.position(7))
         move = "2"
-      elsif board.turn_count >= 4
-      move = [1,2,3,4,5,6,7,8,9].detect{|i| !board.taken?(i)}.to_s
-    end
-
-
+      else
+        move = [1,2,3,4,5,6,7,8,9].detect{|i| !board.taken?(i)}.to_s if move == nil
+      end
     end
   end
 end
