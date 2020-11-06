09762a74c0f10aa00164ff5426e19b0691f81757
Christerle Nounoute <christerlesn@gmail.com>
2020-11-06 04:19:10 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,25 +5,19 @@ module Players
   #  @valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
     def move(board)
       move = nil
-      if board.cells[4] = " "
+      if !board.taken?(5)
         move = "5"
-      elsif board.cells[0] = " "
-        move = "1"
-      elsif board.cells[2] = " "
-        move = "3"
-      elsif board.cells[6] = " "
-        move = "7"
-      elsif board.cells[8] = " "
-        move = "9"
-      elsif board.cells[1] = " "
+      elsif board.turn_count == 1 || board.turn_count == 2
+        move = [1,3,7,9].detect{|i| !board.taken?(i)}.to_s
+      elsif board.turn_count == 3 &&
+        board.position(1) == board.position(9) ||
+        board.position(3) == board.position(7)
         move = "2"
-      elsif board.cells[3] = " "
-        move = "4"
-      elsif board.cells[5] = " "
-        move = "6"
-      elsif board.cells[7] = " "
-        move = "8"
-      end
+      elsif board.turn_count >= 4
+      move = [1,2,3,4,5,6,7,8,9].detect{|i| !board.taken?(i)}.to_s
+    end
+
+
     end
   end
 end
