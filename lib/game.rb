f2406505f959ff4e6996590cf94a54a02c72ad81
Alla Kuzminova <alchik@gmail.com>
2020-12-09 17:56:10 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,8 @@
 class Game
 attr_accessor :board, :player_1, :player_2
 
+@next_player
+
 WIN_COMBINATIONS = [
   [0,1,2], # Top row
   [3,4,5],
@@ -15,81 +17,72 @@ WIN_COMBINATIONS = [
     @player_1 = player_1
     @player_2 = player_2
     @board = board
+    @next_player = player_1
   end
 
-def current_player
-  turns = turn_count
-  return turns % 2 == 0 ? "X" : "O"
-end
-
-def turn_count
-  count = 0
-  self.board.cells.each do  |item|
-    if !(item == " " || item == "" )
-      count += 1
-    end
+  def current_player
+    turns = self.board.turn_count
+    return turns % 2 == 0 ? "X" : "O"
   end
-  count
-end
 
-def won?
-  WIN_COMBINATIONS.detect do |win_combination|
-    (self.board.cells[win_combination[0]] == "X" && self.board.cells[win_combination[1]] == "X" && self.board.cells[win_combination[2]] == "X") ||
-    (self.board.cells[win_combination[0]] == "O" && self.board.cells[win_combination[1]] == "O" && self.board.cells[win_combination[2]] == "O")
- end
-end
-
-def winner
-  if won?
-    if( WIN_COMBINATIONS.detect {|win_combination|
-      (self.board.cells[win_combination[0]] == "X" && self.board.cells[win_combination[1]] == "X" && self.board.cells[win_combination[2]] == "X") })
-      return  "X"
-    else
-      return "O"
-    end
+  def won?
+    WIN_COMBINATIONS.detect do |win_combination|
+      (self.board.cells[win_combination[0]] == "X" && self.board.cells[win_combination[1]] == "X" && self.board.cells[win_combination[2]] == "X") ||
+      (self.board.cells[win_combination[0]] == "O" && self.board.cells[win_combination[1]] == "O" && self.board.cells[win_combination[2]] == "O")
+   end
   end
-   nil
-end
 
-def start
-end
-
-def full?
-  self.board.cells.select{ |item| item != " " && !item.nil? && item != ""}.size == 9
-end
-
-def draw?
-  !(won? || !full?)
-end
-
-def over?
-  won? || full? || draw?
-end
-
-def play
-  while !over?
-        turn
-      end
-      if won?
-        currentwinner = winner
-        puts "Congratulations #{currentwinner}!"
+  def winner
+    if won?
+      if( WIN_COMBINATIONS.detect {|win_combination|
+        (self.board.cells[win_combination[0]] == "X" && self.board.cells[win_combination[1]] == "X" && self.board.cells[win_combination[2]] == "X") })
+        return  "X"
       else
-        puts "Cat's Game!"
+        return "O"
       end
-end
-
-def turn
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  while !valid_move?(index)
-    puts "Please enter 1-9:"
-    input = gets.strip
-    index = input_to_index(input)
+    end
+     nil
   end
-  cur_pl = current_player
-  move(index, cur_pl)
-  display_board
-end
+
+  def start
+  end
+
+  def draw?
+    !(won? || !self.board.full?)
+  end
+
+  def over?
+    won? || self.board.full? || draw?
+  end
+
+  def play
+    while !over?
+          turn
+        end
+        if won?
+          currentwinner = winner
+          puts "Congratulations #{currentwinner}!"
+        else
+          puts "Cat's Game!"
+        end
+  end
+
+  def turn
+    input = next_player.move(board)
+    index = input_to_index(input)
+    while !self.board.valid_move?(index)
+      input = next_player.move(board)
+      index = input_to_index(input)
+    end
+    if next_player == player_1
+      next_player = player_2
+    else
+      next_player = player_1
+    end
+    cur_pl = current_player
+    self.board.update(index, current_player.token)
+    move(index, cur_pl)
+    display_board
+  end
 
 end
