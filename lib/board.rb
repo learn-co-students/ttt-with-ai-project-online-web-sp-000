57ec3940b004257cdecbc00f632b7cccad0e8986
tibraj <tibraj94@gmail.com>
2020-12-11 14:20:57 -0500

    display board

--- lib/board.rb
+++ lib/board.rb
@@ -14,5 +14,9 @@ class Board
         position -= 1
         @cells[x]
     end
-    
+
+    def display 
+        puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]}  \n-------------------------\n #{@cells[3]} | #{@cells[4]} | #{@cells[5]}  \n-------------------------\n #{@cells[6]} | #{@cells[7]} | #{@cells[8]}"
+    end
+
 end
