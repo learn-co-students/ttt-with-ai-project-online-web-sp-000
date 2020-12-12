7e10a877b7dc4cff6d92c80fd5f24741a094d547
Alla Kuzminova <alchik@gmail.com>
2020-12-12 19:09:12 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -5,16 +5,18 @@ class Board
   end
 
   def reset!
-    @cells.clear
+    if self.cells != nil
+      self.cells.clear
+    end
     @cells = Array.new(9, " ")
   end
 
   def display
-    puts " #{@cells[0]} "+"|"+" #{@cells[1]} "+"|"" #{@cells[2]} "
+    puts " #{@cells[0]} "+"|"+" #{@cells[1]} "+"|"+" #{@cells[2]} "
     puts "-----------"
-    puts " #{@cells[3]} "+"|"+" #{@cells4]} "+"|"" #{@cells[5]} "
+    puts " #{@cells[3]} "+"|"+" #{@cells[4]} "+"|"+" #{@cells[5]} "
     puts "-----------"
-    puts " #{@cells[6]} "+"|"+" #{@cells[7]} "+"|"" #{@cells[8]} "
+    puts " #{@cells[6]} "+"|"+" #{@cells[7]} "+"|"+" #{@cells[8]} "
   end
 
   def position(input)
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,8 +2,6 @@ module Players
   class Computer < Player
 
     def move(board)
-
-
       if(board.empty?)
         return 4
       else
@@ -12,16 +10,14 @@ module Players
               item[0] != "O" && item[1] != "O" && item[2] != "O"
               }
               winning_comb[0]
-            end
         else
           winning_comb = Game::WIN_COMBINATIONS.find { |item| item[0] == " " || item[1] == " " || item[2] == " " &&
             item[0] == "O" || item[1] == "O" || item[2] == "O"
             }
-            winning_comb[0]
-
+            move = winning_comb.find {|index| winning_comb[index] == " " }
+            move
         end
       end
     end
-
   end
 end
