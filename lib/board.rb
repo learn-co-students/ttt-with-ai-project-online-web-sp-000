8e93c93148f778918d552f23b0ad80362c5ccb66
GoldenPavilion <rcorystine@gmail.com>
2021-02-01 14:27:14 -0500

    first push

---
+++ lib/board.rb
@@ -0,0 +1,27 @@
+class Board
+
+    attr_accessor :cells
+
+    @cells = []
+
+    def reset!
+        self.cells = Array.new(9, " ")
+    end
+
+    def initialize
+        reset!
+    end
+
+    def display
+        puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
+        puts "-----------"
+        puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
+        puts "-----------"
+        puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
+    end
+
+    def position(user_input)
+        self.cells[user_input.to_i - 1]
+    end
+
+end
