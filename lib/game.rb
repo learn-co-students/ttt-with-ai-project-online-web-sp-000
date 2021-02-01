90ff91cedae077213ca986f8b155c3fe3496eee9
Taylor Treece <taylor.treece@protonmail.com>
2021-02-01 17:01:58 -0600

    completed through #winner method

---
+++ lib/game.rb
@@ -0,0 +1,50 @@
+class Game
+
+    attr_accessor :board, :player_1, :player_2
+
+    WIN_COMBINATIONS = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [6,4,2]
+    ]
+
+    def initialize(player_1=Players::Human.new("X"), player_2=Players::Human.new("O"), board=Board.new)
+        @player_1 = player_1
+        @player_2 = player_2
+        @board = board
+        @board.display
+    end
+
+    def current_player
+        @board.turn_count % 2 == 0 ? @player_1: @player_2
+    end
+
+    def won?
+        WIN_COMBINATIONS.each do |combo|
+            if @board.cells[combo[0]] == @board.cells[combo[1]] &&
+             @board.cells[combo[1]] == @board.cells[combo[2]]
+            return combo
+            end
+        end
+        return false
+    end
+
+    def draw?
+        @board.full? && !won?
+    end
+
+    def over?
+        draw? || won?
+    end
+
+    def winner #Not yet working. It is returning the winner, but not "nil" if a winner is nonexistant. 
+       combo = won?
+       won? ? @board.cells[combo[0]] : nil
+    end
+
+end
