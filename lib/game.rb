f9468a83d09c6bcac56db3feb2d5eebeec724a06
Christopher Black <christopher.black.2209@googlemail.com>
2021-05-28 14:40:18 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,11 @@
 class Game
   WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
-  attr_reader :board, :player_1, :player_2
+  attr_accessor :board, :player_1, :player_2
   
   def initialize(player_1, player_2, board)
+    @player_1 = player_1
+    @player_2 = player_2
     @board = board
-    #@player_1 = Player.new(player_1) 
-    #@player_2 = Player.new(player_2)
   end 
 
   def current_player
@@ -28,7 +28,7 @@ class Game
     
   end 
   
-  def return
+  def turn
   
   end
   
